146f177b8ef3e4c7b56851e642aec76344e92413
jinoosss <jwchoi@onbloc.xyz>
2023-02-02 14:54:24 +0900

    feat: add date utility functions

---
+++ src/common/utils/date-util.ts
@@ -0,0 +1,42 @@
+import dayjs from "dayjs";
+
+const getDateUtcToLocal = (d: any) => {
+	const hasTimezone = `${d}`.includes("Z");
+	const timezoneOffset = new Date().getTimezoneOffset();
+	let currentDate = dayjs(d);
+	if (!hasTimezone) {
+		currentDate = currentDate.subtract(timezoneOffset, "minutes");
+	}
+	return {
+		value: currentDate.format("YYYY-MM-DD HH:mm:ss"),
+		offsetHours: -timezoneOffset / 60,
+	};
+};
+
+export const getDateDiff = (d: any) => {
+	const now = dayjs();
+	const target = dayjs(getDateUtcToLocal(d).value);
+	const diff = now.diff(target, "seconds");
+	const diffM = now.diff(target, "minute");
+	const diffH = now.diff(target, "hours");
+	const diffD = now.diff(target, "day");
+	if (diff < 60) return "less than a minute ago";
+	if (diffM < 2) return "1 min ago";
+	if (diffM >= 2 && diffM <= 59) return `${diffM} mins ago`;
+	if (diffM >= 60 && diffM <= 119) return "1 hour ago";
+	if (diffH >= 2 && diffH <= 23) return `${diffH} hours ago`;
+	if (diffD >= 1 && diffD < 2) return "1 day ago";
+	if (diffD >= 2 && diffD < 31) return `${diffD} days ago`;
+	if (diffD >= 31 && diffD < 61) return "1 month ago";
+	if (diffD >= 61 && diffD < 365) return `${Math.floor(diffD / 30)} months ago`;
+	if (diffD >= 365 && diffD < 730) return "1 year ago";
+	if (diffD >= 730) return `${Math.floor(diffD / 365)} years ago`;
+};
+
+export const getLocalDateString = (d: any) => {
+	const { value, offsetHours } = getDateUtcToLocal(d);
+	const sign = offsetHours > 0 ? "+" : "-";
+	const offsetHoursString =
+		offsetHours === 0 ? "UTC" : `UTC${sign}${Math.abs(offsetHours)}`;
+	return `${value} (${offsetHoursString})`;
+};
