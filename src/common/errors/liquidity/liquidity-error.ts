e21de41a51bab95d81f25e65d43639c3ffa112fc
jinoosss <jwchoi@onbloc.xyz>
2023-02-06 18:37:24 +0900

    feat: add liquidity service errors

---
+++ src/common/errors/liquidity/liquidity-error.ts
@@ -0,0 +1,17 @@
+import { BaseError } from "../base";
+
+const ERROR_VALUE = {
+	NOT_FOUND_LIQUIDITY: {
+		status: 404,
+		type: "Not found liquidity",
+	},
+};
+
+type ErrorType = keyof typeof ERROR_VALUE;
+
+export class LiquidityError extends BaseError {
+	constructor(errorType: ErrorType) {
+		super(ERROR_VALUE[errorType]);
+		Object.setPrototypeOf(this, LiquidityError.prototype);
+	}
+}
