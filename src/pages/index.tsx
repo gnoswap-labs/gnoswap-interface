71dfe5d34e286c5e6100e6294b3a3c530afd1602
jinoosss <jwchoi@onbloc.xyz>
2023-02-07 16:41:48 +0900

    feat: add usecases

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { AccountInfoMapper } from "@/models/account/mapper/account-info-mapper";
 import { AccountInfoModel } from "@/models/account/account-info-model";
 import React, { useContext, useEffect, useState } from "react";
 import { AccountHistoryModel } from "@/models/account/account-history-model";
+import BigNumber from "bignumber.js";
+import { generateId } from "@/common/utils/test-util";
 
 export default function Home() {
 	const [accountInfo, setAccountInfo] = useState<AccountInfoModel>();
@@ -15,8 +17,31 @@ export default function Home() {
 	};
 
 	const onClickGetHistoryButton = () => {
-		accountService
-			.connectAdenaWallet()
+		accountRepository
+			.getNotificationsByAddress("address")
+			.then(res => console.log("connectAdenaWallet : ", res));
+		// accountRepository
+		// 	.getTransactions("123")
+		// 	.then(res => console.log("history : ", res));
+	};
+
+	const onClickCreateHistory = () => {
+		accountRepository
+			.createNotification("address", {
+				txHash: generateId(),
+				status: "SUCCESS",
+				tokenInfo: {
+					amount: {
+						value: BigNumber(0),
+						denom: "ugnot",
+					},
+					tokenId: "1",
+					name: "sfsfs",
+					symbol: "asdfasdf",
+				},
+				txType: 0,
+				createdAt: "",
+			})
 			.then(res => console.log("connectAdenaWallet : ", res));
 		// accountRepository
 		// 	.getTransactions("123")
@@ -28,6 +53,8 @@ export default function Home() {
 			<button onClick={onClickGetAccountButton}>아데나 회원정보 조회</button>
 			<br /> <br /> <br />
 			<button onClick={onClickGetHistoryButton}>히스토리 조회</button>
+			<br /> <br /> <br />
+			<button onClick={onClickCreateHistory}>히스토리 생성</button>
 			{/* <h1>{JSON.stringify(accountInfo, null, 2)}</h1>
 			<h1>{JSON.stringify(history, null, 2)}</h1> */}
 		</div>
