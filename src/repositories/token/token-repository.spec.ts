006fcd2cbb63a38ea17fd834d51bd678ace1d79b
jinoosss <jwchoi@onbloc.xyz>
2023-02-08 14:35:04 +0900

    feat: update token repository

--- src/repositories/token/token-repository.spec.ts
+++ src/repositories/token/token-repository.spec.ts
@@ -12,48 +12,14 @@ describe("getAllTokenMetas", () => {
 	});
 });
 
-describe("getExchangeRateByTokenId", () => {
+describe("getAllExchangeRates", () => {
 	it("tokenId 1 exchange rate is 1", async () => {
 		const tokenId = "1";
 
-		const response = await tokenRepository.getExchangeRateByTokenId(tokenId);
+		const response = await tokenRepository.getAllExchangeRates(tokenId);
 
 		expect(response).toBeTruthy();
-		expect(response.rate).toBeTruthy();
-		expect(response.rate).toBe(1);
-	});
-
-	it("tokenId 2 exchange rate is 1.4", async () => {
-		const tokenId = "2";
-
-		const response = await tokenRepository.getExchangeRateByTokenId(tokenId);
-
-		expect(response).toBeTruthy();
-		expect(response.rate).toBeTruthy();
-		expect(response.rate).toBe(1.4);
-	});
-
-	it("tokenId 3 exchange rate is 0.7", async () => {
-		const tokenId = "3";
-
-		const response = await tokenRepository.getExchangeRateByTokenId(tokenId);
-
-		expect(response).toBeTruthy();
-		expect(response.rate).toBeTruthy();
-		expect(response.rate).toBe(0.7);
-	});
-
-	it("tokenId 4 is not found error", async () => {
-		const tokenId = "4";
-		let error;
-
-		try {
-			const response = await tokenRepository.getExchangeRateByTokenId(tokenId);
-		} catch (e) {
-			error = e;
-		}
-
-		expect(error).toBeTruthy();
+		expect(response.rates).toBeTruthy();
 	});
 });
 
