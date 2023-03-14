3f1976ff094d6d53be537109bad6e4bb5eeb04a6
luvhaeun <luvhaeun2@gmail.com>
2023-03-14 14:19:08 +0900

    dev: Applying an eslint rule

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -3,19 +3,19 @@ const { pathsToModuleNameMapper } = require("ts-jest");
 const { compilerOptions } = require("./tsconfig");
 
 const createJestConfig = nextJest({
-	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-	dir: "./",
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-	roots: ["<rootDir>"],
-	modulePaths: [compilerOptions.baseUrl],
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-		prefix: "<rootDir>/",
-	}),
-	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-	testEnvironment: "jest-environment-jsdom",
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
