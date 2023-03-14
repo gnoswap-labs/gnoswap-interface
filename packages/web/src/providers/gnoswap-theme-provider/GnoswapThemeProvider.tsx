f00ae18ee80bd434182e66abd409c689c8ce3960
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-03-14 12:31:42 +0900

    [GSFE-3] Writing a Footer Component (#19)

--- packages/web/src/providers/gnoswap-theme-provider/GnoswapThemeProvider.tsx
+++ packages/web/src/providers/gnoswap-theme-provider/GnoswapThemeProvider.tsx
@@ -2,17 +2,24 @@ import { ThemeState } from "@/states";
 import { ThemeProvider } from "@emotion/react";
 import { useMemo } from "react";
 import { useRecoilValue } from "recoil";
-import { ChildrenProps } from "../types/global-prop-types";
 import { getTheme } from "@utils/themeUtils";
+import Footer from "@components/common/footer/Footer";
 
-const GnoswapThemeProvider = ({ children }: ChildrenProps) => {
+const GnoswapThemeProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
   const themeKey = useRecoilValue(ThemeState.themeKey);
 
   const theme = useMemo(() => {
     return getTheme(themeKey);
   }, [themeKey]);
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+      <Footer />
+    </ThemeProvider>
+  );
 };
 
 export default GnoswapThemeProvider;
