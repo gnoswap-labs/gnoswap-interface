d7f162e996d89809e63eeff8b2cd60275035361a
luvhaeun <luvhaeun2@gmail.com>
2023-03-14 14:10:12 +0900

    dev: [GSFE-19] Applying an eslint rule

--- packages/web/.eslintrc.js
+++ packages/web/.eslintrc.js
@@ -1,30 +1,29 @@
 module.exports = {
-	root: false,
-	env: {
-		browser: true,
-		es2021: true,
-		jest: true,
-		node: true,
-	},
-	parser: "@typescript-eslint/parser",
-	extends: ["next"],
-	rules: {
-		"indent": ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
-		"quotes": ["error", "double"],
-		"semi": ["error", "always"],
-		"array-element-newline": [
-			"error",
-			{
-				"ArrayExpression": "consistent",
-				"ArrayPattern": { "multiline": true },
-			},
-		],
-	},
-	settings: {
-		"import/external-module-folders": [".yarn"],
-		"react": {
-			"version": "detect",
-		},
-	},
+  root: false,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: ["next"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "array-element-newline": [
+      "error",
+      {
+        ArrayExpression: "consistent",
+        ArrayPattern: { multiline: true },
+      },
+    ],
+  },
+  settings: {
+    "import/external-module-folders": [".yarn"],
+    react: {
+      version: "detect",
+    },
+  },
 };
