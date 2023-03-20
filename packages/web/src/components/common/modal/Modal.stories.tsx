7d684c8f36727a79424fcaad60db33aedbe41956
hyojinDev <gywls3394@gmail.com>
2023-03-20 15:57:50 +0900

    feat: [GSFE-39] Implementing the Common modal

---
+++ packages/web/src/components/common/modal/Modal.stories.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Modal from "./Modal";
+import Button from "../button/Button";
+import { ButtonHierarchy } from "../button/Button.styles";
+
+export default {
+  title: "common/Modal",
+  component: Modal,
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = args => {
+  const [open, setOpen] = useState(true);
+  const toggle = () => {
+    setOpen(prev => !prev);
+  };
+
+  return (
+    <>
+      <Button
+        text="Modal Open"
+        onClick={toggle}
+        style={{
+          width: 200,
+          height: 50,
+          hierarchy: ButtonHierarchy.Primary,
+        }}
+      />
+      <div id="storybook-portal" />
+      {open && (
+        <Modal
+          {...args}
+          exitClick={toggle}
+          selector="storybook-portal"
+          style={{ textColor: "colorWhite", width: 500, height: 150 }}
+        >
+          <Button
+            text="Modal Content 1"
+            style={{
+              fullWidth: true,
+              height: 50,
+              hierarchy: ButtonHierarchy.Primary,
+            }}
+          />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const LeftArrow = Template.bind({});
+LeftArrow.args = {
+  hasLeftArrow: true,
+};
+
+export const LeftText = Template.bind({});
+LeftText.args = {
+  leftText: "Modal Header",
+};
--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -21,6 +21,7 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+          <div id="portal-root" />
           <NextScript />
         </body>
       </Html>
