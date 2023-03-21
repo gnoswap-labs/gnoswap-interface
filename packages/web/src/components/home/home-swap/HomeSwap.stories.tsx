abd06cdeab3cb9a660e534fa985e93b03cb90e24
luvhaeun <luvhaeun2@gmail.com>
2023-03-21 13:23:18 +0900

    feat: [GSFE-41] HomeSwap Component.

--- packages/web/src/components/home/home-swap/HomeSwap.stories.tsx
+++ packages/web/src/components/home/home-swap/HomeSwap.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import HomeSwap from "./HomeSwap";
+import { action } from "@storybook/addon-actions";
 
 export default {
   title: "home/HomeSwap",
@@ -13,4 +14,18 @@ const Template: ComponentStory<typeof HomeSwap> = args => (
 );
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  from: {
+    token: "ETH",
+    amount: "121",
+    price: "$0.00",
+    balence: "0",
+  },
+  to: {
+    token: "BTC",
+    amount: "5,000",
+    price: "$0.00",
+    balence: "0",
+  },
+  swapNow: action("swapNow"),
+};
