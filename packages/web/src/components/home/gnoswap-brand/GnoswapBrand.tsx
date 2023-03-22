93f027f036f2095b60bfb95ccf05869bb2c6a645
luvhaeun <50394458+luvhaeun@users.noreply.github.com>
2023-03-22 11:35:10 +0900

    feat: [GSFE-46] GnoswapBrand Component. (#37)

--- packages/web/src/components/home/gnoswap-brand/GnoswapBrand.tsx
+++ packages/web/src/components/home/gnoswap-brand/GnoswapBrand.tsx
@@ -1,19 +1,79 @@
-import { css } from "@emotion/react";
-import React from "react";
+import IconDiscode from "@components/common/icons/social/IconDiscode";
+import IconGitbook from "@components/common/icons/social/IconGitbook";
+import IconGithub from "@components/common/icons/social/IconGithub";
+import IconMedium from "@components/common/icons/social/IconMedium";
+import IconTwitter from "@components/common/icons/social/IconTwitter";
+import React, { useCallback } from "react";
+import { ValuesType } from "utility-types";
+import { wrapper } from "./GnoswapBrand.styles";
 
-const GnoswapBrand: React.FC = () => {
+export const SNS_TYPE = {
+  GITHUB: "github",
+  GITBOOK: "gitbook",
+  DISCODE: "discode",
+  MEDIUM: "medium",
+  TWITTER: "twitter",
+} as const;
+export type SNS_TYPE = ValuesType<typeof SNS_TYPE>;
+
+interface GnoswapBrandProps {
+  onClickSns: (snsType: SNS_TYPE) => void;
+}
+
+const GnoswapBrand: React.FC<GnoswapBrandProps> = ({ onClickSns }) => {
+  const onClickGithub = useCallback(
+    () => onClickSns(SNS_TYPE.GITHUB),
+    [onClickSns],
+  );
+
+  const onClickGitbook = useCallback(
+    () => onClickSns(SNS_TYPE.GITBOOK),
+    [onClickSns],
+  );
+
+  const onClickDiscode = useCallback(
+    () => onClickSns(SNS_TYPE.DISCODE),
+    [onClickSns],
+  );
+
+  const onClickMedium = useCallback(
+    () => onClickSns(SNS_TYPE.MEDIUM),
+    [onClickSns],
+  );
+
+  const onClickTwitter = useCallback(
+    () => onClickSns(SNS_TYPE.TWITTER),
+    [onClickSns],
+  );
+
   return (
-    <div
-      css={css`
-        border: 1px solid blue;
-      `}
-    >
-      <h2>Swap and Earn on Gnoswap</h2>
-      <h3>the One-stop Gnoland Defi Platform</h3>
-      <div>sns...</div>
-      <p>Gnoswap is an open-source & audited AMM Dex that provides</p>
-      <p>
-        a simplified concentrated-LP experience for increased capital
+    <div css={wrapper}>
+      <h1 className="title">
+        <span>Swap</span> and <span>Earn</span>
+        <br />
+        on Gnoswap
+      </h1>
+      <h4 className="subtitle">the One-stop Gnoland Defi Platform</h4>
+      <div className="sns">
+        <button onClick={onClickGithub}>
+          <IconGithub className="icon" />
+        </button>
+        <button onClick={onClickGitbook}>
+          <IconGitbook className="icon" />
+        </button>
+        <button onClick={onClickDiscode}>
+          <IconDiscode className="icon" />
+        </button>
+        <button onClick={onClickMedium}>
+          <IconMedium className="icon" />
+        </button>
+        <button onClick={onClickTwitter}>
+          <IconTwitter className="icon" />
+        </button>
+      </div>
+      <p className="description">
+        Gnoswap is an open-source & audited AMM Dex that provides
+        <br />a simplified concentrated-LP experience for increased capital
         efficiency.
       </p>
     </div>
