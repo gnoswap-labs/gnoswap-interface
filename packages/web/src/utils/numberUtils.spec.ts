7cf7085e152b1fab5b36a45a4f1fb28228ce9b89
hyojinDev <gywls3394@gmail.com>
2023-03-22 14:28:56 +0900

    feat: [GSFE-38] Implementing the Connect Wallet Component and Dropdown Menu

--- packages/web/src/utils/numberUtils.spec.ts
+++ packages/web/src/utils/numberUtils.spec.ts
@@ -6,13 +6,13 @@ import {
 } from "./numberUtils";
 
 describe("bignumber.js convert to format string", () => {
-  test("123123123.123123123 to 123,123,123.123123123", async () => {
+  test("123123123.123123123 to 123,123,123.123123123", () => {
     const bigNumber = BigNumber("123123123.123123123");
 
     expect(toNumberFormat(bigNumber)).toBe("123,123,123.123123123");
   });
 
-  test("decimal places is 3, 123123123.123123123 to 123,123,123.123", async () => {
+  test("decimal places is 3, 123123123.123123123 to 123,123,123.123", () => {
     const bigNumber = BigNumber("123123123.123123123");
 
     expect(toNumberFormat(bigNumber, 3)).toBe("123,123,123.123");
@@ -20,23 +20,25 @@ describe("bignumber.js convert to format string", () => {
 });
 
 describe("mathSybmolAbsFormat", () => {
-  test("Truncates the number 12345.6789 to 2 decimal places and returns it with a percent notation.", async () => {
-    expect(mathSybmolAbsFormat(12345.6789, 2, false, true)).toBe("+12,345.68%");
+  test("Truncates the number 12345.6789 to 2 decimal places and returns it with a percent notation.", () => {
+    const num = 12345.6789;
+    expect(mathSybmolAbsFormat(num, 2, false, true)).toBe("+12,345.68%");
   });
 
-  test("Truncate the number -12345.6789 to 2 decimal places and return it with a dollar sign", async () => {
-    expect(mathSybmolAbsFormat(-12345.6789, 2, true, false)).toBe(
-      "-$12,345.68",
-    );
+  test("Truncate the number -12345.6789 to 2 decimal places and return it with a dollar sign", () => {
+    const num = -12345.6789;
+    expect(mathSybmolAbsFormat(num, 2, true, false)).toBe("-$12,345.68");
   });
 });
 
 describe("The toUnitFormat function returns a value represented in numeric units and up to two decimal places.", () => {
-  test("12345.6789 to $12.35K", async () => {
-    expect(toUnitFormat(12345.6789, true)).toBe("$12.35K");
+  test("12345.6789 to $12.35K", () => {
+    const num = 12345.6789;
+    expect(toUnitFormat(num, true)).toBe("$12.35K");
   });
 
-  test("-12345.6789 to -12.35K", async () => {
-    expect(toUnitFormat(-12345.6789, false)).toBe("-12.35K");
+  test("-12345.6789 to -12.35K", () => {
+    const num = -12345.6789;
+    expect(toUnitFormat(num, false)).toBe("-12.35K");
   });
 });
