1efe6f8e53a3ea046aeec10105c97cbcf9546616
jinoosss <jwchoi@onbloc.xyz>
2023-03-22 18:28:52 +0900

    feat: [GSFE-43] Writing a asset list component on the Wallet page

---
+++ packages/web/src/components/wallet/asset-info/AssetInfo.spec.tsx
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import GnoswapThemeProvider from "@/providers/gnoswap-theme-provider/GnoswapThemeProvider";
+import AssetInfo from "./AssetInfo";
+
+describe("AssetInfo Component", () => {
+  it("AssetInfo render", () => {
+    const mockProps = {
+      asset: {
+        id: "BTC",
+        logoUri: "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
+        type: "NATIVE" as "GRC20" | "NATIVE",
+        name: "Bitcoin",
+        symbol: "BTC",
+        chain: "Gnoland",
+        balance: "0.000000"
+      },
+      deposit: () => null,
+      withdraw: () => null,
+    };
+
+    render(
+      <RecoilRoot>
+        <GnoswapThemeProvider>
+          <AssetInfo {...mockProps} />
+        </GnoswapThemeProvider>
+      </RecoilRoot>
+    );
+  });
+});
---
+++ packages/web/src/components/wallet/asset-list-table/AssetListTable.spec.tsx
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import GnoswapThemeProvider from "@/providers/gnoswap-theme-provider/GnoswapThemeProvider";
+import AssetListTable from "./AssetListTable";
+
+describe("AssetListTable Component", () => {
+  it("AssetListTable render", () => {
+    const mockProps = {
+      assets: [],
+      error: null,
+      isLoading: false,
+      deposit: () => null,
+      withdraw: () => null,
+    };
+
+    render(
+      <RecoilRoot>
+        <GnoswapThemeProvider>
+          <AssetListTable {...mockProps} />
+        </GnoswapThemeProvider>
+      </RecoilRoot>
+    );
+  });
+});
