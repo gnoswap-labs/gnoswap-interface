82f194b6052f68de86808c232c39083dacb2836e
jinoosss <112360739+jinoosss@users.noreply.github.com>
2023-03-24 06:48:13 +0900

    feat: [GSFE-43] Writing a asset list component on the Wallet page (#39)

---
+++ packages/web/src/components/wallet/asset-info/AssetInfo.styles.ts
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import mixins from "@styles/mixins";
+import { AssetListTableRowWrapper } from "@components/wallet/asset-list-table/AssetListTable.styles";
+
+export const AssetInfoWrapper = styled(AssetListTableRowWrapper)`
+  ${mixins.flexbox("row", "center", "flex-start")};
+
+  & + & {
+    border-top: 1px solid ${({ theme }) => theme.colors.gray50};
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.gray50};
+  }
+
+  span {
+    ${({ theme }) => theme.fonts.body11};
+  }
+
+  .logo {
+    width: 20px;
+    height: 20px;
+    margin-right: 4px;
+  }
+
+  .name {
+    margin-right: 4px;
+  }
+
+  .symbol {
+    ${({ theme }) => theme.fonts.body12};
+    color: ${({ theme }) => theme.colors.gray40};
+  }
+
+  .deposit,
+  .withdraw {
+    ${mixins.flexbox("row", "center", "center")};
+    ${({ theme }) => theme.fonts.body12};
+    color: ${({ theme }) => theme.colors.gray40};
+    gap: 3px;
+
+    svg {
+      width: 16px;
+      height: 16px;
+    }
+
+    path {
+      fill: ${({ theme }) => theme.colors.gray40};
+    }
+  }
+`;
---
+++ packages/web/src/components/wallet/asset-list-table/AssetListTable.stories.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Asset } from "@containers/asset-list-container/AssetListContainer";
+import AssetListTable from "./AssetListTable";
+
+export default {
+  title: "wallet/AssetList/AssetListTable",
+  component: AssetListTable,
+} as ComponentMeta<typeof AssetListTable>;
+
+const Template: ComponentStory<typeof AssetListTable> = args => (
+  <AssetListTable {...args} />
+);
+
+const defaultAssets: Asset[] = [
+  {
+    id: "BTC",
+    logoUri:
+      "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
+    type: "NATIVE",
+    name: "Bitcoin",
+    symbol: "BTC",
+    chain: "Gnoland",
+    balance: "0",
+  },
+  {
+    id: "GNOS",
+    logoUri:
+      "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xB98d4C97425d9908E66E53A6fDf673ACcA0BE986/logo.png",
+    type: "NATIVE",
+    name: "Gnoswap",
+    symbol: "GNOS",
+    chain: "Gnoland",
+    balance: "0",
+  },
+];
+
+export const Default = Template.bind({});
+Default.args = {
+  assets: defaultAssets,
+  error: null,
+  isLoading: false,
+  deposit: action("deposit"),
+  withdraw: action("withdraw"),
+};
+
+export const NoContents = Template.bind({});
+NoContents.args = {
+  assets: [],
+  error: null,
+  isLoading: false,
+  deposit: action("deposit"),
+  withdraw: action("withdraw"),
+};
+
+export const InitialLoading = Template.bind({});
+InitialLoading.args = {
+  assets: [],
+  error: null,
+  isLoading: true,
+  deposit: action("deposit"),
+  withdraw: action("withdraw"),
+};
+
+export const ErrorOccurred = Template.bind({});
+ErrorOccurred.args = {
+  assets: [],
+  error: Error("Error"),
+  isLoading: false,
+  deposit: action("deposit"),
+  withdraw: action("withdraw"),
+};
