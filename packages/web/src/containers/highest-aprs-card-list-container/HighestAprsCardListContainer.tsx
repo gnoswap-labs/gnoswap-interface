98e70e25dd8eb147b863b36f238716191f5b1afc
luvhaeun <50394458+luvhaeun@users.noreply.github.com>
2023-03-24 11:10:55 +0900

    feat: [GSFE-53] Arrange CardList in Home Layout. (#44)

--- packages/web/src/containers/highest-aprs-card-list-container/HighestAprsCardListContainer.tsx
+++ packages/web/src/containers/highest-aprs-card-list-container/HighestAprsCardListContainer.tsx
@@ -1,8 +1,9 @@
+import { highestList } from "@components/home/card-list/card-list-dummy";
 import HighestAprsCardList from "@components/home/highest-aprs-card-list/HighestAprsCardList";
 import React from "react";
 
 const HighestAprsCardListContainer: React.FC = () => {
-  return <HighestAprsCardList />;
+  return <HighestAprsCardList list={highestList} />;
 };
 
 export default HighestAprsCardListContainer;
--- packages/web/src/containers/trending-card-list-container/TrendingCardListContainer.tsx
+++ packages/web/src/containers/trending-card-list-container/TrendingCardListContainer.tsx
@@ -1,8 +1,9 @@
+import { trendingList } from "@components/home/card-list/card-list-dummy";
 import TrendingCardList from "@components/home/trending-card-list/TrendingCardList";
 import React from "react";
 
 const TrendingCardListContainer: React.FC = () => {
-  return <TrendingCardList />;
+  return <TrendingCardList list={trendingList} />;
 };
 
 export default TrendingCardListContainer;
