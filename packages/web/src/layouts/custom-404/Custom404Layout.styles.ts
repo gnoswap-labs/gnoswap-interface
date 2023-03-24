a4d6b079aa407b51f81e113328f20e8ea4809ef2
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-03-24 11:19:06 +0900

    feat: [GSFE-51] Create a 404 page (#45)

---
+++ packages/web/src/layouts/custom-404/Custom404Layout.styles.ts
@@ -0,0 +1,35 @@
+import { css, Theme } from "@emotion/react";
+import mixins from "@styles/mixins";
+
+export const wrapper = (theme: Theme) => css`
+  height: 100%;
+  ${mixins.flexbox("column", "center", "flex-start")}
+  main {
+    ${mixins.flexbox("row", "center", "center")}
+    max-width: 1440px;
+    /* min-height: 100%; */
+    flex-grow: 1;
+    padding: 0 40px;
+    margin: 0 auto;
+  }
+
+  .content-section {
+    ${mixins.flexbox("column", "center", "center")}
+    margin-left: 120px;
+    strong {
+      ${theme.fonts.h1};
+      font-size: 80px;
+      line-height: 96px;
+      color: ${theme.colors.gray10};
+    }
+    p {
+      ${theme.fonts.body3};
+      color: ${theme.colors.gray30};
+      margin: 4px 0px 48px;
+    }
+  }
+
+  .arrow-icon {
+    margin-right: 8px;
+  }
+`;
