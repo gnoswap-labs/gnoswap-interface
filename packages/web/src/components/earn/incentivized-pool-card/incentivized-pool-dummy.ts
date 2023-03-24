9a58e5e82d4e11803062797886f06c000e6339f4
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-03-24 11:17:57 +0900

    feat: [GSFE-50] Implementing the Incentive pools card list component on the Earn page (#43)

---
+++ packages/web/src/components/earn/incentivized-pool-card/incentivized-pool-dummy.ts
@@ -0,0 +1,100 @@
+import { PoolListProps } from "@containers/incentivized-pool-card-list-container/IncentivizedPoolCardListContainer";
+
+export const poolDummy: Array<PoolListProps> = [
+  {
+    logo: [
+      "https://picsum.photos/id/313/36/36",
+      "https://picsum.photos/id/218/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+  {
+    logo: [
+      "https://picsum.photos/id/13/36/36",
+      "https://picsum.photos/id/28/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+  {
+    logo: [
+      "https://picsum.photos/id/158/36/36",
+      "https://picsum.photos/id/268/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+  {
+    logo: [
+      "https://picsum.photos/id/103/36/36",
+      "https://picsum.photos/id/2/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+  {
+    logo: [
+      "https://picsum.photos/id/3/36/36",
+      "https://picsum.photos/id/8/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+  {
+    logo: [
+      "https://picsum.photos/id/19/36/36",
+      "https://picsum.photos/id/58/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+  {
+    logo: [
+      "https://picsum.photos/id/178/36/36",
+      "https://picsum.photos/id/278/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+  {
+    logo: [
+      "https://picsum.photos/id/139/36/36",
+      "https://picsum.photos/id/63/36/36",
+    ],
+    name: ["GNOS", "GNOT"],
+    fee: "0.5% Fee",
+    liquidity: "$524.98M",
+    apr: "108.12%",
+    volume24h: "$200.12M",
+    fees24h: "$10.50k",
+  },
+];
