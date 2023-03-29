2dec4560f99daeee411dfbc212106916608a0947
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-03-29 21:57:25 +0900

    feat: [GSFE-59] Implementing the randge badge common component (#51)

---
+++ packages/web/src/constants/option.constant.ts
@@ -0,0 +1,38 @@
+import { ValuesType } from "utility-types";
+
+export const FEE_RATE_OPTION = {
+  FEE_01: "0.01%",
+  FEE_05: "0.05%",
+  FEE_3: "0.3%",
+  FEE_1: "1%",
+} as const;
+export type POSITION_CONTENT_LABEL = ValuesType<typeof FEE_RATE_OPTION>;
+
+export const STAKED_OPTION = {
+  NONE: "None",
+  STAKED: "Staked",
+  UNSTAKING: "Unstaking",
+  UNSTAKED: "Unstaked",
+} as const;
+export type STAKED_OPTION = ValuesType<typeof STAKED_OPTION>;
+
+export const STATUS_OPTION = {
+  SUCCESS: "SUCCESS",
+  PENDING: "PENDING",
+  FAILED: "FAILED",
+} as const;
+export type STATUS_OPTION = ValuesType<typeof STATUS_OPTION>;
+
+export const ACTIVE_STATUS_OPTION = {
+  NONE: "NONE",
+  ACTIVE: "ACTIVE",
+  IN_ACTIVE: "IN_ACTIVE",
+} as const;
+export type ACTIVE_STATUS_OPTION = ValuesType<typeof ACTIVE_STATUS_OPTION>;
+
+export const RANGE_STATUS_OPTION = {
+  NONE: "NONE",
+  IN: "IN",
+  OUT: "OUT",
+} as const;
+export type RANGE_STATUS_OPTION = ValuesType<typeof RANGE_STATUS_OPTION>;
