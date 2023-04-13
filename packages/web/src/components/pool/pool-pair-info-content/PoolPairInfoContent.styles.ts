69cd662b2389e37585593d883796ff0472ef2c17
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-04-13 09:42:09 +0900

    feat: [GSFE-75] Implementation of the Pool pair information component (#67)

--- packages/web/src/components/pool/pool-pair-info-content/PoolPairInfoContent.styles.ts
+++ packages/web/src/components/pool/pool-pair-info-content/PoolPairInfoContent.styles.ts
@@ -1,4 +1,59 @@
 import { css, type Theme } from "@emotion/react";
 import mixins from "@styles/mixins";
 
-export const wrapper = (theme: Theme) => css``;
+export const wrapper = (theme: Theme) => css`
+  ${mixins.flexbox("row", "center", "center")};
+  width: 100%;
+  height: 155px;
+  background-color: ${theme.colors.gray60};
+  border: 1px solid ${theme.colors.gray50};
+  border-radius: 8px;
+  ${theme.fonts.body12};
+  color: ${theme.colors.gray40};
+  section {
+    ${mixins.flexbox("column", "flex-start", "center")};
+    padding: 24px 36px;
+    height: 100%;
+    flex: 1;
+    gap: 16px;
+    &:not(:first-of-type) {
+      border-left: 1px solid ${theme.colors.gray50};
+    }
+  }
+
+  .has-tooltip {
+    cursor: pointer;
+    transition: color 0.3s ease;
+    &:hover {
+      color: ${theme.colors.brand40};
+    }
+  }
+
+  strong {
+    ${theme.fonts.body2};
+    color: ${theme.colors.gray10};
+  }
+
+  span {
+    &.apr-value {
+      color: ${theme.colors.gray30};
+    }
+    &.status-type {
+      color: ${theme.colors.colorGreen};
+      &.negative {
+        color: ${theme.colors.colorRed};
+      }
+    }
+  }
+  .sub-info-wrap {
+    ${mixins.flexbox("row", "center", "flexstart")};
+
+    .label-info {
+      margin-right: 16px;
+    }
+  }
+
+  .apr-inner-box:first-of-type {
+    margin-right: 49px;
+  }
+`;
