b0f083346fc7bef318bd006f17e773a43090ba11
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-04-13 09:26:28 +0900

    feat: [GSFE-72] Implementing Trending Cryptos Components (#63)

---
+++ packages/web/src/components/token/trending-cryptos/TrendingCryptos.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import TrendingCryptos from "./TrendingCryptos";
+import TrendingCryptoCardListContainer from "@containers/trending-crypto-card-list-container/TrendingCryptoCardListContainer";
+
+export default {
+  title: "token/TrendingCryptos",
+  component: TrendingCryptos,
+} as ComponentMeta<typeof TrendingCryptos>;
+
+const Template: ComponentStory<typeof TrendingCryptos> = args => (
+  <TrendingCryptos {...args} cardList={<TrendingCryptoCardListContainer />} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ packages/web/src/components/token/trending-cryptos/TrendingCryptos.styles.ts
@@ -0,0 +1,18 @@
+import { css, Theme } from "@emotion/react";
+import mixins from "@styles/mixins";
+
+export const wrapper = (theme: Theme) => css`
+  ${mixins.flexbox("column", "flex-start", "center")};
+  width: 100%;
+  background-color: ${theme.colors.colorBlack};
+  border: 1px solid ${theme.colors.gray50};
+  border-radius: 8px;
+  gap: 24px;
+  padding: 24px 0px;
+  h2 {
+    ${theme.fonts.body9};
+    color: ${theme.colors.colorWhite};
+    width: 100%;
+    padding: 0px 24px;
+  }
+`;
