32d0e1ad1c558abc62fc85f9c9f1b46ba7f7d42e
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-04-28 10:26:47 +0900

    feat: [GSFE-124] Create Unstake Liquidity Page Layout and Components (#85)

---
+++ packages/web/src/components/unstake/select-liquidity/SelectLiquidity.styles.ts
@@ -0,0 +1,34 @@
+import { css, type Theme } from "@emotion/react";
+import { inputStyle } from "@components/unstake/unstake-liquidity/UnstakeLiquidity.styles";
+import mixins from "@styles/mixins";
+import { fonts } from "@constants/font.constant";
+
+export const wrapper = (theme: Theme) => css`
+  ${mixins.flexbox("column", "center", "center")};
+  width: 100%;
+  gap: 4px;
+  .checked-all-wrap {
+    ${inputStyle(theme)};
+    ${mixins.flexbox("row", "center", "flex-start")}
+    width: 100%;
+    height: 32px;
+    padding: 0px 15px;
+    color: ${theme.colors.gray30};
+    ${fonts.body12};
+    .period-label {
+      width: 60px;
+      text-align: right;
+      margin-right: 70px;
+      margin-left: auto;
+    }
+    .liquidity-label {
+      width: 80px;
+      text-align: right;
+    }
+  }
+  ul {
+    ${mixins.flexbox("column", "center", "center")};
+    gap: 4px;
+    width: 100%;
+  }
+`;
---
+++ packages/web/src/components/unstake/select-liquidity/SelectLiquidity.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import SelectLiquidityItem from "@components/unstake/select-liquidity-item/SelectLiquidityItem";
+import { wrapper } from "./SelectLiquidity.styles";
+
+interface SelectLiquidityProps {
+  list: any[];
+  checkedList: string[];
+  onCheckedItem: (checked: boolean, tokenId: string) => void;
+  onCheckedAll: (checked: boolean) => void;
+  checkedAll: boolean;
+}
+
+const SelectLiquidity: React.FC<SelectLiquidityProps> = ({
+  list,
+  checkedList,
+  onCheckedItem,
+  onCheckedAll,
+  checkedAll,
+}) => {
+  return (
+    <div css={wrapper}>
+      <div className="checked-all-wrap">
+        <input
+          id="checkbox-all"
+          type="checkbox"
+          checked={checkedAll}
+          onChange={e => onCheckedAll(e.target.checked)}
+        />
+        <label htmlFor="checkbox-all" className="select-all-label">
+          Select All
+        </label>
+        <span className="period-label">Period</span>
+        <span className="liquidity-label">Liquidity</span>
+      </div>
+      <ul>
+        {list.map((item, idx) => (
+          <SelectLiquidityItem
+            item={item}
+            checkedList={checkedList}
+            onCheckedItem={onCheckedItem}
+            key={idx}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SelectLiquidity;
