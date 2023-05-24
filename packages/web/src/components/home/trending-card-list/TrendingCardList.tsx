8595326cd066fccc3e31325ed4d72570754ed484
hjlee0 <hjlee@onbloc.xyz>
2023-05-24 13:41:49 +0900

    fix: [GSFE-116] Add Icons to the Top 3 Tokens cards in the main menu

--- packages/web/src/components/home/trending-card-list/TrendingCardList.tsx
+++ packages/web/src/components/home/trending-card-list/TrendingCardList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CardList, { type ListProps } from "@components/home/card-list/CardList";
 import { wrapper } from "./TrendingCardList.styles";
+import IconFlame from "@components/common/icons/IconFlame";
 interface TrendingCardListProps {
   list: Array<ListProps>;
 }
@@ -8,7 +9,10 @@ interface TrendingCardListProps {
 const TrendingCardList: React.FC<TrendingCardListProps> = ({ list }) => {
   return (
     <div css={wrapper}>
-      <h2>Trending</h2>
+      <h2>
+        <IconFlame className="icon-flame" />
+        Trending
+      </h2>
       <CardList list={list} />
     </div>
   );
