49c1c76b1febfb247f8ecf18d75d24d102c4d4e0
hjlee0 <110363675+hjlee0@users.noreply.github.com>
2023-05-26 10:31:14 +0900

    fix: [GSFE-101] Fix margin and padding on the main page (#97)

--- packages/web/src/layouts/home-layout/HomeLayout.styles.ts
+++ packages/web/src/layouts/home-layout/HomeLayout.styles.ts
@@ -2,43 +2,39 @@ import mixins from "@/styles/mixins";
 import { css, Theme } from "@emotion/react";
 
 export const wrapper = (theme: Theme) => css`
-  .hero-section {
-    ${mixins.flexbox("row", "center", "space-between")};
-    flex-wrap: wrap;
-
-    width: 100%;
-    max-width: 1440px;
-    margin: 100px auto;
-    padding: 0 40px;
-
-    .brand-container {
-      width: 706px;
-    }
-
-    .swap-container {
-      width: 520px;
-    }
-
-    .card-list {
-      width: 100%;
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-template-rows: auto;
-      grid-gap: 24px;
-      grid-template-columns: repeat(3, 1fr);
-
-      margin-top: 120px;
-    }
-  }
-
+  .hero-section,
   .tokens-section {
     ${mixins.flexbox("row", "center", "space-between")};
     flex-wrap: wrap;
     width: 100%;
     max-width: 1440px;
     margin: 0 auto;
-    padding: 40px 0px 100px;
+  }
 
-    ${mixins.flexbox("row", "center", "space-between")};
+  .hero-section {
+    padding: 100px 40px 30px;
+  }
+
+  .tokens-section {
+    padding: 30px 40px 100px;
+  }
+
+  .brand-container {
+    width: 706px;
+  }
+
+  .swap-container {
+    width: 520px;
+  }
+
+  .card-list {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    grid-gap: 24px;
+    grid-template-columns: repeat(3, 1fr);
+
+    margin-top: 120px;
   }
 `;
