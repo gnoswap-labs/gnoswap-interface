537a565db5697f3b01ba6e7eb4c5f8566a2d6505
jinoosss <jwchoi@onbloc.xyz>
2023-07-12 12:12:29 +0900

    feat: Add sorting feature to a column in a token list

--- packages/web/src/components/home/token-list-table/TokenListTable.tsx
+++ packages/web/src/components/home/token-list-table/TokenListTable.tsx
@@ -1,25 +1,39 @@
 import {
+  SortOption,
   TABLE_HEAD,
   type Token,
 } from "@containers/token-list-container/TokenListContainer";
-import React from "react";
+import React, { useCallback } from "react";
 import TokenInfo from "@components/home/token-info/TokenInfo";
 import { cx } from "@emotion/css";
 import TableSkeleton from "@components/common/table-skeleton/TableSkeleton";
 import { noDataText, TableHeader, TableWrapper } from "./TokenListTable.styles";
 import { TOKEN_INFO, TOKEN_TD_WIDTH } from "@constants/skeleton.constant";
+import IconTriangleArrowDown from "@components/common/icons/IconTriangleArrowDown";
+import IconTriangleArrowUp from "@components/common/icons/IconTriangleArrowUp";
 
 interface TokenListTableProps {
   tokens: Token[];
   isFetched: boolean;
+  sortOption?: SortOption;
   onClickTableHead: (head: TABLE_HEAD) => void;
 }
 
 const TokenListTable: React.FC<TokenListTableProps> = ({
   tokens,
+  sortOption,
   onClickTableHead,
   isFetched,
 }) => {
+
+  const isAscendingOption = useCallback((head: TABLE_HEAD) => {
+    return sortOption?.key === head && sortOption.direction === "asc";
+  }, [sortOption]);
+
+  const isDescendingOption = useCallback((head: TABLE_HEAD) => {
+    return sortOption?.key === head && sortOption.direction === "desc";
+  }, [sortOption]);
+
   return (
     <TableWrapper>
       <div className="scroll-wrapper">
@@ -28,10 +42,11 @@ const TokenListTable: React.FC<TokenListTableProps> = ({
             <TableHeader
               key={idx}
               className={cx({ left: [0, 1].includes(idx) })}
-              onClick={() => onClickTableHead(head)}
               tdWidth={TOKEN_TD_WIDTH[idx]}
             >
-              <span className={Object.keys(TABLE_HEAD)[idx].toLowerCase()}>
+              <span className={Object.keys(TABLE_HEAD)[idx].toLowerCase()} onClick={() => onClickTableHead(head)}>
+                {isAscendingOption(head) && <IconTriangleArrowUp className="icon asc" />}
+                {isDescendingOption(head) && <IconTriangleArrowDown className="icon desc" />}
                 {head}
               </span>
             </TableHeader>
