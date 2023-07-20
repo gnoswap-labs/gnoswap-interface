40e1b39624bc573d0bdf52dd2871006c4761fa97
jtpark <pjt0014@gmail.com>
2023-07-20 15:22:55 +0900

    feat: Creating and Structuring Dashboard Page

---
+++ packages/web/src/containers/dashboard-info-container/DashboardInfoContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardInfoContainer: React.FC = () => {
+  return <div>DashboardInfoContainer TBD</div>;
+};
+
+export default DashboardInfoContainer;
---
+++ packages/web/src/containers/tvl-chart-container/TvlChartContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TvlChartContainer: React.FC = () => {
+  return <div>TvlChartContainer TBD</div>;
+};
+
+export default TvlChartContainer;
