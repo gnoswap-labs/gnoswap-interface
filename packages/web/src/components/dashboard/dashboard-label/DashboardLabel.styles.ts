7926f8674ddd1c5460ca203121df9dee393071bd
jtpark <pjt0014@gmail.com>
2023-07-24 15:16:15 +0900

    feat: Develop DashboardInfo Component

---
+++ packages/web/src/components/dashboard/dashboard-label/DashboardLabel.styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import mixins from "@styles/mixins";
+import { fonts } from "@constants/font.constant";
+
+export const WalletBalanceDetailInfoTooltipContent = styled.div`
+  ${mixins.flexbox("column", "flex-start", "flex-start")};
+  width: calc(300px - 32px);
+  ${fonts.body12};
+  color: ${({ theme }) => theme.color.text15};
+  background-color: ${({ theme }) => theme.color.background14};
+`;
--- packages/web/src/containers/dashboard-info-container/DashboardInfoContainer.tsx
+++ packages/web/src/containers/dashboard-info-container/DashboardInfoContainer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import DashboardInfo from "@components/dashboard/dashboard-info/DashboardInfo";
 
 const DashboardInfoContainer: React.FC = () => {
-  return <div>DashboardInfoContainer TBD</div>;
+  return <DashboardInfo />;
 };
 
 export default DashboardInfoContainer;
