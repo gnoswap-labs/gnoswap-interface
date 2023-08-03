fc276d6581931befe641f2282c9c017d3909add2
jtpark <pjt0014@gmail.com>
2023-08-03 17:28:04 +0900

    feat: Header Responsive Web Develop

--- packages/web/src/components/common/notification-button/NotificationButton.styles.ts
+++ packages/web/src/components/common/notification-button/NotificationButton.styles.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { media } from "@styles/media";
 import mixins from "@styles/mixins";
 
 export const NotificationWrapper = styled.div`
@@ -11,8 +12,17 @@ export const AlertButton = styled.button`
   height: 36px;
   border-radius: 4px;
   transition: all 0.3s ease;
-  .notification-icon * {
-    fill: ${({ theme }) => theme.color.icon05};
+  .notification-icon {
+    width: 36px;
+    height: 36px;
+    padding: 4px;
+    ${media.tablet} {
+      width: 32px;
+      height: 32px;
+    }
+    * {
+      fill: ${({ theme }) => theme.color.icon05};
+    }
   }
   &:hover {
     .notification-icon * {
