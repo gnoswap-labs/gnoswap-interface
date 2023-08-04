a71923c30d0c68e0b88b8d9a0f7476d8210ab500
juntaepark <112064254+juntaepark@users.noreply.github.com>
2023-08-04 18:32:11 +0900

    feat: Fix the main logo size (#164)

---
+++ packages/web/src/components/common/icons/IconHeaderLogo.tsx
@@ -0,0 +1,32 @@
+const IconHeaderLogo = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="37"
+    viewBox="0 0 32 37"
+    fill="none"
+    className={className}
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M7.89433 5.6145L15.7431 0.5L31.4989 10.686L23.662 15.7962L7.89433 5.6145ZM7.89375 20.4607V5.60114L-0.000717163 10.6863V25.5864V25.5869L-0.000284195 25.5866L15.7493 35.7369L22.871 31.1447L23.6153 30.6657L7.89432 20.4603L7.89375 20.4607ZM15.9062 13.1316L8.05739 18.2461L23.8073 28.3639L31.4993 23.1626L15.9062 13.1316ZM23.8074 28.3638L31.5 23.1626V25.5472L23.6444 30.6521L23.8074 28.3638Z"
+      fill="url(#paint0_linear_6611_66589)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_6611_66589"
+        x1="15.7496"
+        y1="0.5"
+        x2="15.7496"
+        y2="36.5"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#233DBD" />
+        <stop offset="1" stop-color="#0059FF" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+export default IconHeaderLogo;
