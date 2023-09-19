fd0ba1531ac155b4f39619cb590339bf2e114f5e
jinoosss <jwchoi@onbloc.xyz>
2023-09-19 19:56:21 +0900

    [GSW-20] feat: Develop Pool Incentivise Pool Selector component

---
+++ packages/web/src/components/incentivize/pool-incentivize-select-pool-item/PoolIncentivizeSelectPoolItem.styles.ts
@@ -0,0 +1,53 @@
+import { fonts } from "@constants/font.constant";
+import styled from "@emotion/styled";
+
+export const PoolIncentivizeSelectPoolItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: auto;
+  align-items: center;
+  padding: 16px 0;
+  cursor: pointer;
+
+  .main-content-wrapper {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: auto;
+    gap: 5px;
+    align-items: center;
+
+    .token-pair-name {
+      height: 14px;
+      color: ${({ theme }) => theme.color.text03};
+      ${fonts.body12}
+    }
+  }
+
+  .sub-content-wrapper {
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+    height: auto;
+    align-items: center;
+
+    .liquidity {
+      height: 14px;
+      color: ${({ theme }) => theme.color.text03};
+      ${fonts.body12}
+    }
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.hover02};
+  }
+`;
+
+export const PoolIncentivizeSelectPoolItemDefaultWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  color: ${({ theme }) => theme.color.text02};
+  align-items: center;
+`;
