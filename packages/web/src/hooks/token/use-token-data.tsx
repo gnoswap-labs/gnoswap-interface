066843b5f7e2ab36408df64ae3c754c90d5190fc
jinoosss <jwchoi@onbloc.xyz>
2023-10-12 16:23:08 +0900

    [GSW-323] feat: Change Token price data

--- packages/web/src/hooks/token/use-token-data.tsx
+++ packages/web/src/hooks/token/use-token-data.tsx
@@ -2,11 +2,12 @@ import { useGnoswapContext } from "@hooks/common/use-gnoswap-context";
 import { useWallet } from "@hooks/wallet/use-wallet";
 import { CardListTokenInfo } from "@models/common/card-list-item-info";
 import { TokenModel } from "@models/token/token-model";
+import { TokenPriceModel } from "@models/token/token-price-model";
 import { TokenState } from "@states/index";
 import { evaluateExpressionToNumber } from "@utils/rpc-utils";
 import BigNumber from "bignumber.js";
 import { useAtom } from "jotai";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 export const useTokenData = () => {
   const { account } = useWallet();
@@ -15,10 +16,16 @@ export const useTokenData = () => {
   const [tokenPrices, setTokenPrices] = useAtom(TokenState.tokenPrices);
   const [balances, setBalances] = useAtom(TokenState.balances);
 
+  useEffect(() => {
+    if (rpcProvider && account) {
+      updateBalances();
+    }
+  }, [rpcProvider, account]);
+
   const trendingTokens: CardListTokenInfo[] = useMemo(() => {
     const sortedTokens = tokens.sort((t1, t2) => {
       if (tokenPrices[t1.priceId] && tokenPrices[t2.priceId]) {
-        return tokenPrices[t2.priceId].volume - tokenPrices[t1.priceId].volume;
+        return BigNumber(tokenPrices[t2.priceId].volume).toNumber() - BigNumber(tokenPrices[t1.priceId].volume).toNumber();
       }
       if (tokenPrices[t2.priceId]) {
         return 1;
@@ -31,8 +38,8 @@ export const useTokenData = () => {
     return sortedTokens.map(token => (
       tokenPrices[token.priceId] ? {
         token,
-        upDown: tokenPrices[token.priceId].changedBefore1D > 0 ? "up" : "down",
-        content: `${BigNumber(tokenPrices[token.priceId].changedBefore1D).toFixed()}%`
+        upDown: BigNumber(tokenPrices[token.priceId].change1d).isPositive() ? "up" : "down",
+        content: `${BigNumber(tokenPrices[token.priceId].change1d).toFixed()}%`
       } : {
         token,
         upDown: "none",
@@ -65,7 +72,11 @@ export const useTokenData = () => {
 
   async function updateTokenPrices() {
     const response = await tokenRepository.getTokenPrices();
-    setTokenPrices(response.prices);
+    const priceMap = response.prices.reduce<Record<string, TokenPriceModel>>((prev, current) => {
+      prev[current.path] = current;
+      return prev;
+    }, {});
+    setTokenPrices(priceMap);
   }
 
   async function updateBalances() {
@@ -85,7 +96,7 @@ export const useTokenData = () => {
     const balances: Record<string, number | null> = {};
     fetchResults.forEach((result, index) => {
       if (index < tokens.length) {
-        balances[tokens[index].priceId] = result;
+        balances[tokens[index].path] = result;
       }
     });
     setBalances(balances);
