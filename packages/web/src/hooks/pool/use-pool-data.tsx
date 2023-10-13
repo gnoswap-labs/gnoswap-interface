67da391ec5cf0edfd1b4d3d71bae92c8518a8468
jinoosss <jwchoi@onbloc.xyz>
2023-10-13 13:54:09 +0900

    [GSW-416] feat: Integrate Pools API

--- packages/web/src/hooks/pool/use-pool-data.tsx
+++ packages/web/src/hooks/pool/use-pool-data.tsx
@@ -1,5 +1,8 @@
+import { PoolPosition } from "@containers/earn-my-position-container/EarnMyPositionContainer";
 import { useGnoswapContext } from "@hooks/common/use-gnoswap-context";
 import { CardListPoolInfo } from "@models/common/card-list-item-info";
+import { PoolCardInfo } from "@models/pool/info/pool-card-info";
+import { PoolMapper } from "@models/pool/mapper/pool-mapper";
 import { PoolState } from "@states/index";
 import { useAtom } from "jotai";
 import { useMemo } from "react";
@@ -7,6 +10,12 @@ import { useMemo } from "react";
 export const usePoolData = () => {
   const { poolRepository } = useGnoswapContext();
   const [pools, setPools] = useAtom(PoolState.pools);
+  const [isFetchedPools, setIsFetchedPools] = useAtom(PoolState.isFetchedPools);
+  const [isFetchedPositions, setIsFetchedPositions] = useAtom(PoolState.isFetchedPositions);
+
+  const poolListInfos = useMemo(() => {
+    return pools.map(PoolMapper.toListInfo);
+  }, [pools]);
 
   const higestAPRs: CardListPoolInfo[] = useMemo(() => {
     const sortedTokens = pools.sort((p1, p2) => {
@@ -19,13 +28,34 @@ export const usePoolData = () => {
     }));
   }, [pools]);
 
+  const myPositions: PoolPosition[] = useMemo(() => {
+    return [];
+  }, []);
+  async function updatePositions() {
+    setIsFetchedPositions(true);
+  }
+
+  const incentivizedPools: PoolCardInfo[] = useMemo(() => {
+    return pools
+      .map(PoolMapper.toCardInfo)
+      .filter(info => info.incentiveType === "Incentivized");
+  }, [pools]);
+
   async function updatePools() {
     const response = await poolRepository.getPools();
     setPools(response.pools);
+    setIsFetchedPools(true);
   }
 
   return {
+    isFetchedPools,
     higestAPRs,
+    isFetchedPositions,
+    myPositions,
+    pools,
+    poolListInfos,
+    incentivizedPools,
     updatePools,
+    updatePositions,
   };
 };
