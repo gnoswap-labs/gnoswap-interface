8bb57aeff7e177b5c744662e1b8465061b13d0e7
jinoosss <jwchoi@onbloc.xyz>
2023-12-15 10:55:15 +0900

    fix: Fix a build error

--- packages/web/src/repositories/swap/swap-router-repository-mock.ts
+++ packages/web/src/repositories/swap/swap-router-repository-mock.ts
@@ -2,8 +2,6 @@ import { SwapRouterRepository } from "./swap-router-repository";
 import { EstimateSwapRouteRequest } from "./request/estimate-swap-route-request";
 import { EstimateSwapRouteResponse } from "./response/estimate-swap-route-response";
 import { PoolRPCModel } from "@models/pool/pool-rpc-model";
-import { UnwrapTokenRequest } from "./request/unwrap-token-request";
-import { WrapTokenRequest } from "./request/wrap-token-request";
 
 export class SwapRouterRepositoryMock implements SwapRouterRepository {
   public pools: PoolRPCModel[] = [];
