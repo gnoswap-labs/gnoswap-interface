84c2df588ff8b0fd206349e064b7545dacbac4a0
HoangPham99 <pqhoang.dev@gmail.com>
2023-12-17 22:27:35 +0700

    [GSW-673] feat: fix unit test

--- packages/web/src/components/common/notification-button/NotificationButton.spec.tsx
+++ packages/web/src/components/common/notification-button/NotificationButton.spec.tsx
@@ -4,6 +4,7 @@ import GnoswapThemeProvider from "@providers/gnoswap-theme-provider/GnoswapTheme
 import { Provider as JotaiProvider } from "jotai";
 import { DEVICE_TYPE } from "@styles/media";
 import GnoswapServiceProvider from "@providers/gnoswap-service-provider/GnoswapServiceProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 describe("NotificationButton Component", () => {
   it("Notification button", () => {
@@ -11,14 +12,26 @@ describe("NotificationButton Component", () => {
       breakpoint: DEVICE_TYPE.WEB,
     };
 
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnMount: false,
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false,
+        },
+      },
+    });
+
     render(
-      <JotaiProvider>
-        <GnoswapServiceProvider>
-          <GnoswapThemeProvider>
-            <NotificationButton {...mockProps} />
-          </GnoswapThemeProvider>
-        </GnoswapServiceProvider>
-      </JotaiProvider>,
+      <QueryClientProvider client={queryClient}>
+        <JotaiProvider>
+          <GnoswapServiceProvider>
+            <GnoswapThemeProvider>
+              <NotificationButton {...mockProps} />
+            </GnoswapThemeProvider>
+          </GnoswapServiceProvider>
+        </JotaiProvider>
+      </QueryClientProvider>,
     );
   });
 });
