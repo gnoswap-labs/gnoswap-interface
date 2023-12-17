63fa683db6b4e82aa394be9732f3d12a7d6bf9a9
HoangPham99 <pqhoang.dev@gmail.com>
2023-12-17 22:13:48 +0700

    [GSW-673] feat: fix unit test

--- packages/web/src/components/common/notification-button/NotificationButton.spec.tsx
+++ packages/web/src/components/common/notification-button/NotificationButton.spec.tsx
@@ -3,6 +3,7 @@ import { render } from "@testing-library/react";
 import GnoswapThemeProvider from "@providers/gnoswap-theme-provider/GnoswapThemeProvider";
 import { Provider as JotaiProvider } from "jotai";
 import { DEVICE_TYPE } from "@styles/media";
+import GnoswapServiceProvider from "@providers/gnoswap-service-provider/GnoswapServiceProvider";
 
 describe("NotificationButton Component", () => {
   it("Notification button", () => {
@@ -12,9 +13,11 @@ describe("NotificationButton Component", () => {
 
     render(
       <JotaiProvider>
-        <GnoswapThemeProvider>
-          <NotificationButton {...mockProps} />
-        </GnoswapThemeProvider>
+        <GnoswapServiceProvider>
+          <GnoswapThemeProvider>
+            <NotificationButton {...mockProps} />
+          </GnoswapThemeProvider>
+        </GnoswapServiceProvider>
       </JotaiProvider>,
     );
   });
