7133a39f4d6561d1b0d7f6915943f5b3d54a85c0
hoailp <phuochoaileqn@gmail.com>
2023-12-19 17:10:57 +0700

    feat: implement transaction status for remove position

--- packages/web/src/containers/remove-position-modal-container/RemovePositionModalContainer.tsx
+++ packages/web/src/containers/remove-position-modal-container/RemovePositionModalContainer.tsx
@@ -1,10 +1,15 @@
 import RemovePositionModal from "@components/remove/remove-position-modal/RemovePositionModal";
+import PositionStatus from "@containers/submit-position-modal-container/PositionModalStatus";
+import { PositionResponse } from "@containers/submit-position-modal-container/SubmitPositionModalContainer";
 import { useClearModal } from "@hooks/common/use-clear-modal";
 import { useGnoswapContext } from "@hooks/common/use-gnoswap-context";
+import { useNotice } from "@hooks/common/use-notice";
 import { useWallet } from "@hooks/wallet/use-wallet";
 import { PoolPositionModel } from "@models/position/pool-position-model";
+import { makeRandomId, parseJson } from "@utils/common";
 import { useRouter } from "next/router";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
+import { TNoticeType } from "src/context/NoticeContext";
 
 interface RemovePositionModalContainerProps {
   positions: PoolPositionModel[];
@@ -18,8 +23,15 @@ const RemovePositionModalContainer = ({
   const router = useRouter();
   const clearModal = useClearModal();
 
+  const [isConfirm, setIsConfirm] = useState<boolean>(false);
+  const [positionResult, setPositionResult] = useState<PositionResponse>(null);
+
+  const { setNotice } = useNotice();
+
   const close = useCallback(() => {
     clearModal();
+    setPositionResult(null);
+    setIsConfirm(false);
   }, [clearModal]);
 
   const onSubmit = useCallback(async () => {
@@ -28,17 +40,91 @@ const RemovePositionModalContainer = ({
       return null;
     }
     const lpTokenIds = positions.map(position => position.id);
-    const result = await positionRepository.removeLiquidity({
-      lpTokenIds,
-      caller: address
-    }).catch(() => null);
-    if (result) {
-      clearModal();
+
+    setIsConfirm(true);
+    setPositionResult(null);
+
+    try {
+      const result = await positionRepository.removeLiquidity({
+        lpTokenIds,
+        caller: address,
+      });
+
+      setPositionResult({
+        success: true,
+        hash: result ?? "",
+      });
+
+      setNotice(null, {
+        timeout: 50000,
+        type: "pending",
+        closeable: true,
+        id: makeRandomId(),
+      });
+
+      setTimeout(() => {
+        setNotice(null, {
+          timeout: 50000,
+          type: "success" as TNoticeType,
+          closeable: true,
+          id: makeRandomId(),
+        });
+      }, 1000);
+    } catch (error) {
+      if (error instanceof Error) {
+        const { code } = parseJson(error?.message);
+        setPositionResult({
+          success: code === 0 ? true : false,
+          code,
+        });
+
+        if (code !== 4000) {
+          setNotice(null, {
+            timeout: 50000,
+            type: "pending",
+            closeable: true,
+            id: makeRandomId(),
+          });
+
+          setTimeout(() => {
+            setNotice(null, {
+              timeout: 50000,
+              type:
+                code === 0
+                  ? ("success" as TNoticeType)
+                  : ("error" as TNoticeType),
+              closeable: true,
+              id: makeRandomId(),
+            });
+          }, 1000);
+        }
+      }
+    }
+  }, [account?.address, positionRepository, positions, setNotice]);
+
+  const onCloseTransaction = () => {
+    setIsConfirm(false);
+    close();
+    if (positionResult?.success) {
       router.back();
     }
-  }, [account?.address, clearModal, positionRepository, positions, router]);
+  };
 
-  return <RemovePositionModal positions={positions} close={close} onSubmit={onSubmit} />;
+  if (isConfirm)
+    return (
+      <PositionStatus
+        close={onCloseTransaction}
+        positionResult={positionResult}
+      />
+    );
+
+  return (
+    <RemovePositionModal
+      positions={positions}
+      close={close}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default RemovePositionModalContainer;
