7b44250b77307f87346031e04e8942f5885ec2fc
jinoosss <jwchoi@onbloc.xyz>
2024-01-17 22:14:05 +0900

    fix: Fix a tests

--- packages/web/src/components/common/sub-menu-button/SubMenuButton.spec.tsx
+++ packages/web/src/components/common/sub-menu-button/SubMenuButton.spec.tsx
@@ -1,14 +1,19 @@
-import SubMenuButton from "./SubMenuButton";
 import { render } from "@testing-library/react";
-import GnoswapThemeProvider from "@providers/gnoswap-theme-provider/GnoswapThemeProvider";
 import { Provider as JotaiProvider } from "jotai";
+import GnoswapThemeProvider from "@providers/gnoswap-theme-provider/GnoswapThemeProvider";
+import SubMenuButton from "./SubMenuButton";
 
 describe("SubMenuButton Component", () => {
   it("SubMenuButton render", () => {
+    const args = {
+      sideMenuToggle: false,
+      onSideMenuToggle: () => { return; }
+    };
+
     render(
       <JotaiProvider>
         <GnoswapThemeProvider>
-          <SubMenuButton sideMenuToggle={true} onSideMenuToggle={() => {}} />
+          <SubMenuButton {...args} />
         </GnoswapThemeProvider>
       </JotaiProvider>,
     );
