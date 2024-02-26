2335fdb98ee527181567eefcbdd1033e97db82de
khiemldk <khiemld28@gmail.com>
2024-02-26 12:05:39 +0700

    [GSW-MAIN] fix: remove bech

--- packages/web/src/utils/validation-utils.ts
+++ packages/web/src/utils/validation-utils.ts
@@ -1,24 +1,9 @@
 import { ErrorResponse } from "@common/errors/response";
-import { bech32 } from "bech32";
 
-function fromBech32(
-  address: string,
-  limit = Infinity,
-): { readonly prefix: string; readonly data: Uint8Array } {
-  const decodedAddress = bech32.decode(address, limit);
-  return {
-    prefix: decodedAddress.prefix,
-    data: new Uint8Array(bech32.fromWords(decodedAddress.words)),
-  };
-}
-
-export function addressValidationCheck(address: string): boolean {
-  try {
-    const publicKey = fromBech32(address);
-    return Boolean(publicKey?.prefix);
-  } catch {
-    return false;
-  }
+export function addressValidationCheck(v: string): boolean {
+  const startStringCheck = /^g/;
+  const atozAndNumberCheck = /^[a-z0-9]{40}$/;
+  return startStringCheck.test(v) && atozAndNumberCheck.test(v) ? true : false;
 }
 
 export function isErrorResponse(resposne: any): resposne is ErrorResponse {
