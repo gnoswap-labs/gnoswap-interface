f440a10bd65446a546321e2f2e655ed6706578f9
beescuit <pengsoon20@gmail.com>
2024-03-15 17:42:06 +0900

    style : [GSW-948] remvove unused style file

--- packages/web/src/components/leaderboard/leaderboard-list-header/LeaderboardListTable.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import mixins from "@styles/mixins";
-
-export const ListHeaderWrapper = styled.div`
-  ${mixins.flexbox("row", "", "space-between")};
-  width: 100%;
-  color: ${({ theme }) => theme.color.text04};
-`;
