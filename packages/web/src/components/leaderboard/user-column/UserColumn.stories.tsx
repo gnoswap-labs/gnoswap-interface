c1b0913234b38f557983c50264380731578dc2dc
beescuit <pengsoon20@gmail.com>
2024-03-26 17:34:18 +0900

    fix [GSW-991] Update storybook testcases args for UserColumn component

--- packages/web/src/components/leaderboard/user-column/UserColumn.stories.tsx
+++ packages/web/src/components/leaderboard/user-column/UserColumn.stories.tsx
@@ -20,14 +20,26 @@ export const Gold: StoryObj<typeof UserColumn> = {
 };
 
 export const Silver: StoryObj<typeof UserColumn> = {
-  args: { rank: 2, user: "g1j0q...8auvm" },
+  args: {
+    rank: 2,
+    user: "g112...xnf5",
+    address: "g1122u9x3d2p11mlqm3yrzqijqnee5oni2l7xnf5",
+    hide: true,
+    me: false,
+  },
   parameters: {
     backgrounds: { default: "light" },
   },
 };
 
 export const Bronze: StoryObj<typeof UserColumn> = {
-  args: { rank: 3, user: "g1j0q...8auvm" },
+  args: {
+    rank: 3,
+    user: "g112...xnf5",
+    address: "g1122u9x3d2p11mlqm3yrzqijqnee5oni2l7xnf5",
+    hide: true,
+    me: false,
+  },
   parameters: {
     backgrounds: { default: "light" },
   },
