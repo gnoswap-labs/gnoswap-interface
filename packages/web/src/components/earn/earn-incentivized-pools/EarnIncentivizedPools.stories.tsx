509ec4be754fa20c3b9f783917a2f6348b782ce0
phuc-varmeta <phucth@var-meta.com>
2024-04-17 10:13:03 +0700

    chore: minor fix

--- packages/web/src/components/earn/earn-incentivized-pools/EarnIncentivizedPools.stories.tsx
+++ packages/web/src/components/earn/earn-incentivized-pools/EarnIncentivizedPools.stories.tsx
@@ -26,7 +26,7 @@ Default.args = {
   cardList: <IncentivizedPoolCardList
     currentIndex={1}
     incentivizedPools={[cardInfo]}
-    isFetched={true}
+    isPoolFetched={true}
     routeItem={action("routeItem")}
     mobile={false}
     loadMore={false}
