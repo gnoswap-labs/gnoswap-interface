712e13b3e54ac695e6b19c1a007bbc104bac0776
jinoosss <jwchoi@onbloc.xyz>
2024-06-04 00:35:57 +0900

    fix: Fix a wallet issue

--- packages/web/src/components/common/my-position-card/MyPositionCard.tsx
+++ packages/web/src/components/common/my-position-card/MyPositionCard.tsx
@@ -145,10 +145,11 @@ const MyPositionCard: React.FC<MyPositionCardProps> = ({
   const minTickLabel = useMemo(() => {
     return minTickRate * -1 > 1000
       ? ">999%"
-      : `${minTickRate < 0 ? "+" : ""}${Math.abs(minTickRate) > 0 && Math.abs(minTickRate) < 1
-        ? "<1"
-        : Math.round(minTickRate * -1)
-      }%`;
+      : `${minTickRate < 0 ? "+" : ""}${
+          Math.abs(minTickRate) > 0 && Math.abs(minTickRate) < 1
+            ? "<1"
+            : Math.round(minTickRate * -1)
+        }%`;
   }, [minTickRate]);
 
   const maxTickLabel = useMemo(() => {
@@ -158,8 +159,9 @@ const MyPositionCard: React.FC<MyPositionCardProps> = ({
 
     return maxTickRate >= 1000
       ? ">999%"
-      : `${maxTickRate > 0 && maxTickRate >= 1 ? "+" : ""}${Math.abs(maxTickRate) < 1 ? "<1" : Math.round(maxTickRate)
-      }%`;
+      : `${maxTickRate > 0 && maxTickRate >= 1 ? "+" : ""}${
+          Math.abs(maxTickRate) < 1 ? "<1" : Math.round(maxTickRate)
+        }%`;
   }, [maxTickRate]);
 
   const tickRange = useMemo(() => {
@@ -182,7 +184,7 @@ const MyPositionCard: React.FC<MyPositionCardProps> = ({
     }
     return (
       ((position.tickLower - currentTick) / (max - currentTick)) *
-      (GRAPH_WIDTH / 2) +
+        (GRAPH_WIDTH / 2) +
       GRAPH_WIDTH / 2
     );
   }, [GRAPH_WIDTH, position.pool.currentTick, position.tickLower, tickRange]);
@@ -200,7 +202,7 @@ const MyPositionCard: React.FC<MyPositionCardProps> = ({
     }
     return (
       ((position.tickUpper - currentTick) / (max - currentTick)) *
-      (GRAPH_WIDTH / 2) +
+        (GRAPH_WIDTH / 2) +
       GRAPH_WIDTH / 2
     );
   }, [GRAPH_WIDTH, position.pool.currentTick, position.tickUpper, tickRange]);
@@ -296,7 +298,9 @@ const MyPositionCard: React.FC<MyPositionCardProps> = ({
 
   const dailyEarning = useMemo(() => {
     const value = position.reward.reduce((acc, current) => {
-      const currentTokenPrice = Number(tokenPrices?.[current.rewardToken.priceID].usd ?? 0);
+      const currentTokenPrice = tokenPrices?.[current.rewardToken.priceID]
+        ? Number(tokenPrices?.[current.rewardToken.priceID].usd)
+        : 0;
 
       return acc + Number(current.accuReward1D ?? 0) * currentTokenPrice;
     }, 0);
@@ -336,8 +340,8 @@ const MyPositionCard: React.FC<MyPositionCardProps> = ({
                 inRange === null
                   ? RANGE_STATUS_OPTION.NONE
                   : inRange
-                    ? RANGE_STATUS_OPTION.IN
-                    : RANGE_STATUS_OPTION.OUT
+                  ? RANGE_STATUS_OPTION.IN
+                  : RANGE_STATUS_OPTION.OUT
               }
             />
           </div>
