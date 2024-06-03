2c22aa729572a87134c508aba52a179c28bbf7d2
jinoosss <jwchoi@onbloc.xyz>
2024-06-03 22:41:00 +0900

    feat: Handle blocked pages
    * feat: Handle blocked pages
    
    * fix: Fix a UI

--- packages/web/src/constants/header.constant.ts
+++ packages/web/src/constants/header.constant.ts
@@ -2,20 +2,26 @@ export const HEADER_NAV = [
   {
     title: "Swap",
     path: "/swap",
+    iconType: null,
     subPath: ["/tokens/"],
   },
   {
     title: "Earn",
     path: "/earn",
+    iconType: null,
     subPath: ["/earn/pool/", "/earn/add", "/earn/stake"],
   },
   {
     title: "Wallet",
     path: "/wallet",
+    iconType: null,
+    subPath: [],
   },
   {
     title: "Leaderboard",
     path: "/leaderboard",
+    iconType: null,
+    subPath: [],
   },
 ];
 
@@ -24,11 +30,13 @@ export const SIDE_MENU_NAV = [
     title: "Dashboard",
     path: "/dashboard",
     iconType: "PULSE",
+    subPath: [],
   },
   {
     title: "Governance",
     path: "/governance",
     iconType: "ACCOUNT_USER",
+    subPath: [],
   },
 ] as const;
 
