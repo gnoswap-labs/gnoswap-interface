6ef16addf0f076c187037dcdb74989e274eeece3
phucvarmeta <phucth@var-meta.com>
2024-07-01 17:30:20 +0700

    feat: decrease/remove with unwrap, ui fixes

--- packages/web/src/hooks/reposition/use-reposition-handle.tsx
+++ packages/web/src/hooks/reposition/use-reposition-handle.tsx
@@ -57,7 +57,7 @@ export const useRepositionHandle = () => {
   const { slippage, changeSlippage } = useSlippage();
   const { connected, account } = useWallet();
   const { positions } = usePositionData({
-    poolPath: encryptId(poolPath)
+    poolPath: encryptId(poolPath),
   });
   const { openModal: openConfirmModal, update: updateConfirmModalData } =
     useTransactionConfirmModal();
@@ -101,8 +101,8 @@ export const useRepositionHandle = () => {
     return selectedPosition?.closed
       ? RANGE_STATUS_OPTION.NONE
       : inRange
-        ? RANGE_STATUS_OPTION.IN
-        : RANGE_STATUS_OPTION.OUT;
+      ? RANGE_STATUS_OPTION.IN
+      : RANGE_STATUS_OPTION.OUT;
   }, [selectedPosition, inRange]);
 
   const aprFee = useMemo(() => {
@@ -191,13 +191,13 @@ export const useRepositionHandle = () => {
       : 1 / selectPool.currentPrice;
     const originPrices = ordered
       ? {
-        minPrice: tickToPrice(selectedPosition.tickLower),
-        maxPrice: tickToPrice(selectedPosition.tickUpper),
-      }
+          minPrice: tickToPrice(selectedPosition.tickLower),
+          maxPrice: tickToPrice(selectedPosition.tickUpper),
+        }
       : {
-        minPrice: tickToPrice(-1 * selectedPosition.tickUpper),
-        maxPrice: tickToPrice(-1 * selectedPosition.tickLower),
-      };
+          minPrice: tickToPrice(-1 * selectedPosition.tickUpper),
+          maxPrice: tickToPrice(-1 * selectedPosition.tickLower),
+        };
     const depositPrices = {
       minPrice: selectPool.minPrice,
       maxPrice: selectPool.maxPrice,
@@ -221,7 +221,7 @@ export const useRepositionHandle = () => {
       originDepositAmounts.amountA.toString(),
     ).dividedBy(
       Number(originDepositAmounts.amountA.toString()) +
-      Number(originDepositAmounts.amountB.toString()),
+        Number(originDepositAmounts.amountB.toString()),
     );
     const currentDepositRatioBN = BigNumber(
       Number(depositAmounts.amountA),
@@ -386,6 +386,7 @@ export const useRepositionHandle = () => {
             selectedPosition.pool.tokenA.path,
             selectedPosition.pool.tokenB.path,
           ],
+          existWrappedToken: true,
         })
         .catch(() => null);
     }, [selectedPosition, positionRepository, address]);
