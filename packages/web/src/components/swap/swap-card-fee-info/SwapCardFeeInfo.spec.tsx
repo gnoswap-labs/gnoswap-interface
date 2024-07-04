930e77e19f99fedbce1183e4c7d624141b9d1c78
phucvarmeta <phucth@var-meta.com>
2024-07-04 14:26:51 +0700

    fix: unit test issue

--- packages/web/src/components/swap/swap-card-fee-info/SwapCardFeeInfo.spec.tsx
+++ packages/web/src/components/swap/swap-card-fee-info/SwapCardFeeInfo.spec.tsx
@@ -3,6 +3,8 @@ import { Provider as JotaiProvider } from "jotai";
 import GnoswapThemeProvider from "@providers/gnoswap-theme-provider/GnoswapThemeProvider";
 import SwapCardFeeInfo from "./SwapCardFeeInfo";
 import { SwapSummaryInfo } from "@models/swap/swap-summary-info";
+import { SwapTokenInfo } from "@models/swap/swap-token-info";
+import { PriceImpactStatus } from "@hooks/swap/use-swap-handler";
 
 const swapSummaryInfo: SwapSummaryInfo = {
   tokenA: {
@@ -13,9 +15,10 @@ const swapSummaryInfo: SwapSummaryInfo = {
     path: "gno.land/r/foo",
     decimals: 4,
     symbol: "FOO",
-    logoURI: "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
+    logoURI:
+      "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
     priceID: "gno.land/r/foo",
-    address: ""
+    address: "",
   },
   tokenB: {
     type: "grc20",
@@ -25,9 +28,10 @@ const swapSummaryInfo: SwapSummaryInfo = {
     path: "gno.land/r/foo",
     decimals: 4,
     symbol: "FOO",
-    logoURI: "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
+    logoURI:
+      "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
     priceID: "gno.land/r/foo",
-    address: ""
+    address: "",
   },
   swapDirection: "EXACT_IN",
   swapRate: 1.14,
@@ -35,13 +39,54 @@ const swapSummaryInfo: SwapSummaryInfo = {
   priceImpact: 0.3,
   guaranteedAmount: {
     amount: 45124,
-    currency: "GNOT"
+    currency: "GNOT",
   },
   gasFee: {
     amount: 0.000001,
-    currency: "GNOT"
+    currency: "GNOT",
   },
-  gasFeeUSD: 0.1
+  gasFeeUSD: 0.1,
+  swapRate1USD: 0,
+  swapRateAction: "ATOB",
+};
+
+const swapTokenInfo: SwapTokenInfo = {
+  tokenA: {
+    chainId: "dev",
+    createdAt: "2023-10-17T05:58:00+09:00",
+    name: "Foo",
+    address: "g1evezrh92xaucffmtgsaa3rvmz5s8kedffsg469",
+    path: "gno.land/r/foo",
+    decimals: 4,
+    symbol: "FOO",
+    logoURI:
+      "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
+    type: "grc20",
+    priceID: "gno.land/r/foo",
+  },
+  tokenAAmount: "0",
+  tokenABalance: "0",
+  tokenAUSD: 0,
+  tokenAUSDStr: "0",
+  tokenB: {
+    chainId: "dev",
+    createdAt: "2023-10-17T05:58:00+09:00",
+    name: "Foo",
+    address: "g1evezrh92xaucffmtgsaa3rvmz5s8kedffsg469",
+    path: "gno.land/r/foo",
+    decimals: 4,
+    symbol: "FOO",
+    logoURI:
+      "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
+    type: "grc20",
+    priceID: "gno.land/r/foo",
+  },
+  tokenBAmount: "0",
+  tokenBBalance: "0",
+  tokenBUSD: 0,
+  tokenBUSDStr: "0",
+  direction: "EXACT_IN",
+  slippage: "0",
 };
 
 describe("SwapCardFeeInfo Component", () => {
@@ -50,6 +95,9 @@ describe("SwapCardFeeInfo Component", () => {
       openedRouteInfo: false,
       toggleRouteInfo: () => null,
       swapSummaryInfo,
+      swapTokenInfo,
+      isLoading: false,
+      priceImpactStatus: "HIGH" as PriceImpactStatus,
     };
 
     render(
