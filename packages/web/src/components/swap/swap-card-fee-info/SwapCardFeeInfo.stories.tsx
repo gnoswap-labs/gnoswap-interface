7bf11420adff965b9ef812b2d1f0128d915f7a0d
phucvarmeta <phucth@var-meta.com>
2024-07-11 16:04:37 +0700

    fix: ui issue

--- packages/web/src/components/swap/swap-card-fee-info/SwapCardFeeInfo.stories.tsx
+++ packages/web/src/components/swap/swap-card-fee-info/SwapCardFeeInfo.stories.tsx
@@ -90,45 +90,6 @@ const swapTokenInfo: SwapTokenInfo = {
   slippage: "0",
 };
 
-const swapTokenInfo: SwapTokenInfo = {
-  tokenA: {
-    chainId: "dev",
-    createdAt: "2023-10-17T05:58:00+09:00",
-    name: "Foo",
-    address: "g1evezrh92xaucffmtgsaa3rvmz5s8kedffsg469",
-    path: "gno.land/r/foo",
-    decimals: 4,
-    symbol: "FOO",
-    logoURI:
-      "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
-    type: "grc20",
-    priceID: "gno.land/r/foo",
-  },
-  tokenAAmount: "0",
-  tokenABalance: "0",
-  tokenAUSD: 0,
-  tokenAUSDStr: "0",
-  tokenB: {
-    chainId: "dev",
-    createdAt: "2023-10-17T05:58:00+09:00",
-    name: "Foo",
-    address: "g1evezrh92xaucffmtgsaa3rvmz5s8kedffsg469",
-    path: "gno.land/r/foo",
-    decimals: 4,
-    symbol: "FOO",
-    logoURI:
-      "https://raw.githubusercontent.com/onbloc/gno-token-resource/main/grc20/images/gno_land_r_foo.svg",
-    type: "grc20",
-    priceID: "gno.land/r/foo",
-  },
-  tokenBAmount: "0",
-  tokenBBalance: "0",
-  tokenBUSD: 0,
-  tokenBUSDStr: "0",
-  direction: "EXACT_IN",
-  slippage: "0",
-};
-
 export default {
   title: "swap/SwapCardFeeInfo",
   component: SwapCardFeeInfo,
