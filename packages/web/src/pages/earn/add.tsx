1730184d41246516737da8dc9de9036bf4c14745
jinoosss <jwchoi@onbloc.xyz>
2024-07-18 21:04:48 +0900

    chore: add multilingualism keys

--- packages/web/src/pages/earn/add.tsx
+++ packages/web/src/pages/earn/add.tsx
@@ -21,7 +21,11 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 export async function getServerSideProps({ locale }: { locale: string }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["HeaderFooter", "common"])),
+      ...(await serverSideTranslations(locale, [
+        "HeaderFooter",
+        "common",
+        "Swap",
+      ])),
     },
   };
 }
