979c18462199c4332f3062370d86fdf0b0fe0c64
Aeron Park <21303012+AkydA@users.noreply.github.com>
2024-08-01 12:57:55 +0900

    fix: avoid build error

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
@@ -11,6 +13,9 @@ const nextConfig = {
       use: ["@svgr/webpack", "url-loader"],
     });
     return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
