5c42a328fd443704294345a984ca41ebec010743
Aeron Park <21303012+AkydA@users.noreply.github.com>
2024-09-04 15:16:06 +0900

    chore: add shortcut index file

--- packages/web/src/providers/gnoswap-service-provider/GnoswapServiceProvider.tsx
+++ packages/web/src/providers/gnoswap-service-provider/GnoswapServiceProvider.tsx
@@ -38,8 +38,7 @@ import {
   PositionRepository,
   PositionRepositoryImpl,
 } from "@repositories/position";
-import { StatusRepository } from "@repositories/status/status-repository";
-import { StatusRepositoryImpl } from "@repositories/status/status-repository-impl";
+import { StatusRepository, StatusRepositoryImpl } from "@repositories/status";
 import {
   SwapRouterRepository,
   SwapRouterRepositoryImpl,
