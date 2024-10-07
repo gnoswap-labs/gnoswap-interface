2fc51ef2b14b8dc3b52f8bf40862f2ba8eae5554
yjin <devyzyn@gmail.com>
2024-10-07 18:44:05 +0900

    feat(launchpad) : Active Project Data section UI

--- packages/web/src/views/launchpad/components/launchpad-active-projects/launchpad-active-projects-content/launchpad-active-projects-card-list/launchpad-active-project-card/launchpad-active-project-card-data/LaunchpadActiveProjectCardData.styles.ts
+++ packages/web/src/views/launchpad/components/launchpad-active-projects/launchpad-active-projects-content/launchpad-active-projects-card-list/launchpad-active-project-card/launchpad-active-project-card-data/LaunchpadActiveProjectCardData.styles.ts
@@ -9,5 +9,22 @@ export const ActiveProjectCardDataWrapper = styled.div`
     ${mixins.flexbox("column", "flex-start", "center")};
     gap: 8px;
     width: 100%;
+    color: ${({theme}) => theme.color.text02}
+  }
+  .data-title {
+    font-size: 14px;
+    font-weight: 400;
+  }
+  .data {
+    font-size: 18px;
+    font-weight: 500;
+  }
+  .badge {
+    font-size: 12px;
+    font-weight: 400;
+    padding: 2px 6px;
+    border-radius: 4px;
+    border: 1px solid var(--Dark-Border-Gray400-Border-3, #596782);
+    background: var(--global-opacity-gray-600-opacity-05, rgba(20, 26, 41, 0.50));
   }
 `;
--- packages/web/src/views/launchpad/components/launchpad-active-projects/launchpad-active-projects-content/launchpad-active-projects-card-list/launchpad-active-project-card/launchpad-active-project-card-data/LaunchpadActiveProjectCardData.tsx
+++ packages/web/src/views/launchpad/components/launchpad-active-projects/launchpad-active-projects-content/launchpad-active-projects-card-list/launchpad-active-project-card/launchpad-active-project-card-data/LaunchpadActiveProjectCardData.tsx
@@ -6,19 +6,19 @@ const LaunchpadActiveProjectCardData = () => {
   return (
     <ActiveProjectCardDataWrapper>
       <div className="data-box">
-        <span>Pool1</span>
-        <span>189.71% APR</span>
-        <span>1 Month</span>
+        <span className="data-title">Pool1</span>
+        <span className="data">189.71% APR</span>
+        <span className="badge">1 Month</span>
       </div>
       <div className="data-box">
-        <span>Pool1</span>
-        <span>189.71% APR</span>
-        <span>1 Month</span>
+        <span className="data-title">Pool1</span>
+        <span className="data">189.71% APR</span>
+        <span className="badge">1 Month</span>
       </div>
       <div className="data-box">
-        <span>Pool1</span>
-        <span>189.71% APR</span>
-        <span>1 Month</span>
+        <span className="data-title">Pool1</span>
+        <span className="data">189.71% APR</span>
+        <span className="badge">1 Month</span>
       </div>
     </ActiveProjectCardDataWrapper>
   );
