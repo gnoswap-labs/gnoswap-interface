2658ba040edccefbc78c480f87a6afa8840e36af
yjin <devyzyn@gmail.com>
2024-10-17 18:32:00 +0900

    refactor(launchpad) : memoization Launchpad main image

--- packages/web/src/components/common/icons/IconLaunchpadMain.tsx
+++ packages/web/src/components/common/icons/IconLaunchpadMain.tsx
@@ -2,35 +2,39 @@
  * Todo: Delete This code.
  */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
 
 /* eslint-disable @next/next/no-img-element */
 import { ThemeKeys } from "@styles/ThemeTypes";
 
-const IconLaunchpadMain = ({
-  className,
-  themeKey = "dark",
-}: {
+interface IconLaunchpadMainProps {
   className?: string;
   themeKey?: ThemeKeys;
-}) => {
-  /**
-   * TODO: The style object below is commented out but may be needed later.
-   * [2024-10-01] [To apply different styles for light mode and dark mode]
-   *
-   * const styleObject = {
-   *   opacity: themeKey === "dark" ? "60%" : "80%",
-   *   mixBlendMode: themeKey === "dark" ? "hard-light" : "darken",
-   * };
-   */
+}
 
-  return (
-    <img
-      src="/img-launchpad.png"
-      alt="launchpad-main-img"
-      className={className}
-      // style={styleObject}
-    />
-  );
-};
+const IconLaunchpadMain: React.FC<IconLaunchpadMainProps> = React.memo(
+  ({ className, themeKey = "dark" }) => {
+    /**
+     * TODO: The style object below is commented out but may be needed later.
+     * [2024-10-01] [To apply different styles for light mode and dark mode]
+     *
+     * const styleObject = {
+     *   opacity: themeKey === "dark" ? "60%" : "80%",
+     *   mixBlendMode: themeKey === "dark" ? "hard-light" : "darken",
+     * };
+     */
+
+    return (
+      <img
+        src="/img-launchpad.png"
+        alt="launchpad-main-img"
+        className={className}
+        // style={styleObject}
+      />
+    );
+  },
+);
+
+IconLaunchpadMain.displayName = "IconLaunchpadMain";
 
 export default IconLaunchpadMain;
