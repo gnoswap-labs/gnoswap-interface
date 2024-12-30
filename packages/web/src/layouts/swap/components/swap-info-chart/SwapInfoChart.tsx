89948809c165261a71fa876170824c411bd69612
yjin <devyzyn@gmail.com>
2024-12-30 15:58:05 +0900

    feat: [GSW-2033] display default token when no tokens are selected

--- packages/web/src/layouts/swap/components/swap-info-chart/SwapInfoChart.tsx
+++ packages/web/src/layouts/swap/components/swap-info-chart/SwapInfoChart.tsx
@@ -6,15 +6,19 @@ import { SwapState } from "@states/index";
 import { SwapInfoChartWrapper } from "./SwapInfoChart.styles";
 import SwapTokenInfo from "./swap-token-info/SwapTokenInfo";
 import { Divider } from "@components/common/divider/divider";
+import { GNOT_TOKEN_DEFAULT } from "@common/values/token-constant";
 
 const SwapInfoChart = () => {
   const swapValue = useAtomValue(SwapState.swap);
   const { tokenA, tokenB } = swapValue;
+  const DEFAULT_TOKEN = GNOT_TOKEN_DEFAULT;
 
+  const hasNoTokens = !tokenA && !tokenB;
   const isTokenPairSelected = tokenA && tokenB;
 
   return (
     <SwapInfoChartWrapper>
+      {hasNoTokens && <SwapTokenInfo token={DEFAULT_TOKEN} />}
       {tokenA && <SwapTokenInfo token={tokenA} />}
       {isTokenPairSelected && <Divider />}
       {tokenB && <SwapTokenInfo token={tokenB} />}
