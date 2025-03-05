ecd7fca8feaf99ce136ec3f7f52af7a8cdfae73b
yjin <devyzyn@gmail.com>
2025-03-05 10:48:33 +0900

    fix: [GSW-2128] Build Error

--- packages/web/src/repositories/swap/swap-repository.ts
+++ packages/web/src/repositories/swap/swap-repository.ts
@@ -1,6 +1,6 @@
 import { SwapHistoryRequest } from "./request/swap-history-request";
-import { SwapHistoryResponse } from "./response/swap-history-response";
+import { SwapHistoryItem } from "./response/swap-history-response";
 
 export interface SwapRepository {
-  getSwapHistory: (request: SwapHistoryRequest) => Promise<SwapHistoryResponse[]>;
+  getSwapHistory: (request: SwapHistoryRequest) => Promise<SwapHistoryItem[]>;
 }
