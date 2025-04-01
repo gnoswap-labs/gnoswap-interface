a715d6b85d18a82d28d8ffe8db8764d529d1ea03
yjin <devyzyn@gmail.com>
2025-04-01 17:08:15 +0900

    fix: TokenPrice Chart decimal

--- packages/web/src/containers/token-list-container/TokenListContainer.tsx
+++ packages/web/src/containers/token-list-container/TokenListContainer.tsx
@@ -223,7 +223,7 @@ const TokenListContainer: React.FC = () => {
         const latestGraphPrice =
           tempTokenPrice?.last7d?.length > 0
             ? tempTokenPrice.last7d[tempTokenPrice.last7d.length - 1].price
-            : tempTokenPrice.pricesBefore.latestPrice;
+            : tempTokenPrice.pricesBefore?.latestPrice;
         const graphStatus = checkPositivePrice(transferData.pricesBefore?.latestPrice, latestGraphPrice).status;
 
         const data30D = checkPositivePrice(transferData.pricesBefore?.latestPrice, transferData.pricesBefore?.price30d);
