4810359588e325df346809fab4cc2e630c302917
yjin <devyzyn@gmail.com>
2025-04-09 14:26:39 +0900

    fix: [GSW-2199] optimize LeaderboardTableRow display value calculation

--- packages/web/src/layouts/leaderboard-layout/components/leaderboard-table-row/LeaderboardTableRow.tsx
+++ packages/web/src/layouts/leaderboard-layout/components/leaderboard-table-row/LeaderboardTableRow.tsx
@@ -10,7 +10,12 @@ import PointComposition from "../point-composition/PointComposition";
 import UserColumn from "../user-column/UserColumn";
 import { LeaderboardUser } from "@repositories/leaderboard/response/common/types";
 import { isLeaderboardHidden } from "@utils/leaderboard-utils";
-import { numberToFormat } from "@utils/string-utils";
+import { numberToInteger } from "@utils/string-utils";
+
+const formatUsdValue = (value: string | number) => {
+  if (value == null) return "-";
+  return `$${numberToInteger(value)}`;
+};
 
 const LeaderboardTableRow = ({
   data,
@@ -26,21 +31,18 @@ const LeaderboardTableRow = ({
   const Hover = isMe ? HoverSection : HoverOnBgSection;
   const TableWrapper = isMe ? WrapperHoverBackground : Wrapper;
 
-  const displaySwapVolume = React.useMemo(() => {
-    if (!data.swapFeeUsd) return "-";
-    return `$${numberToFormat(data.swapFeeUsd)}`;
-  }, [data.swapFeeUsd]);
+  const displayValues = React.useMemo(() => {
+    const stakingTotal =
+      data.stakingRewardsUsd && data.governanceRewardsUsd
+        ? Number(data.stakingRewardsUsd) + Number(data.governanceRewardsUsd)
+        : data.stakingRewardsUsd ?? data.governanceRewardsUsd ?? null;
 
-  const displayPositionValue = React.useMemo(() => {
-    if (!data.providedLiquidityFeeUsd) return "-";
-    return `$${numberToFormat(data.providedLiquidityFeeUsd)}`;
-  }, [data.providedLiquidityFeeUsd]);
-
-  const displayStakingValue = React.useMemo(() => {
-    if (!data.stakingRewardsUsd && !data.governanceRewardsUsd) return "-";
-    const totalRewards = Number(data.stakingRewardsUsd) + Number(data.governanceRewardsUsd);
-    return `$${numberToFormat(totalRewards)}`;
-  }, [data.stakingRewardsUsd, data.governanceRewardsUsd]);
+    return {
+      swap: formatUsdValue(data.swapFeeUsd),
+      position: formatUsdValue(data.providedLiquidityFeeUsd),
+      staking: formatUsdValue(stakingTotal),
+    };
+  }, [data.swapFeeUsd, data.providedLiquidityFeeUsd, data.stakingRewardsUsd, data.governanceRewardsUsd]);
 
   return (
     <TableWrapper>
@@ -50,10 +52,10 @@ const LeaderboardTableRow = ({
       </Hover>
       <Hover style={{ cursor: "auto" }}>
         <TableColumn tdWidth={tdWidths.at(2)} style={{ justifyContent: "flex-start" }}>
-          {displaySwapVolume}
+          {displayValues.swap}
         </TableColumn>
-        <TableColumn tdWidth={tdWidths.at(3)}>{displayPositionValue}</TableColumn>
-        <TableColumn tdWidth={tdWidths.at(4)}>{displayStakingValue}</TableColumn>
+        <TableColumn tdWidth={tdWidths.at(3)}>{displayValues.position}</TableColumn>
+        <TableColumn tdWidth={tdWidths.at(4)}>{displayValues.staking}</TableColumn>
         <TableColumn tdWidth={tdWidths.at(5)}>
           <PointComposition
             totalPoint={data.totalPoint}
