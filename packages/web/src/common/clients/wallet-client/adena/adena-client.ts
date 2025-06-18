e77baa5d377936dc1026bc11623e81bfd66b2b00
yjin <devyzyn@gmail.com>
2025-06-18 18:24:37 +0900

    fix: [GSW-2058] Adena Sign

--- packages/web/src/common/clients/wallet-client/adena/adena-client.ts
+++ packages/web/src/common/clients/wallet-client/adena/adena-client.ts
@@ -1,8 +1,7 @@
 import { createTimeout } from "@common/utils/client-util";
 import { DEFAULT_GAS_WANTED } from "@common/values";
-import { Provider, Tx, TxSignature } from "@gnolang/tm2-js-client";
-import { documentToTx } from "@utils/transaction-utils";
-import { Document } from "src/types/transaction-messages.types";
+import { Tx, TxSignature } from "@gnolang/tm2-js-client";
+// import { documentToTx } from "@utils/transaction-utils";
 import { WalletType } from "src/types/wallet.types";
 import {
   AccountInfo,
@@ -65,13 +64,14 @@ export class AdenaClient implements WalletClient {
     return accountInfo;
   }
 
-  public async sign(provider: Provider, document: Document): Promise<{ signed: Tx; signature: TxSignature[] }> {
-    const tx = documentToTx(document);
+  public async sign(): Promise<{ signed: Tx; signature: TxSignature[] }> {
+    throw new Error("Sign method not implemented for Adena wallet");
+    // const tx = documentToTx(document);
 
-    return {
-      signed: tx,
-      signature: [],
-    };
+    // return {
+    //   signed: tx,
+    //   signature: [],
+    // };
   }
 
   public addEstablishedSite = (sitename: string): Promise<WalletResponse> => {
