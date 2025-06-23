4987d2abb660e609fb500653b22b27df02bf4bf1
yjin <devyzyn@gmail.com>
2025-06-23 17:16:31 +0900

    feat: [GSW-2058] GasUsed Buffer

--- packages/web/src/hooks/gas/use-get-estimate-gas-info.ts
+++ packages/web/src/hooks/gas/use-get-estimate-gas-info.ts
@@ -8,6 +8,7 @@ import { makeEstimateGasTransaction } from "@utils/transaction-utils";
 import { Document } from "src/types/transaction-messages.types";
 import { GasInfo } from "@hooks/gas";
 import { QUERY_KEY } from "@query/query-keys";
+import { DEFAULT_GAS_WANTED, GAS_WANTED_BUFFER_MULTIPLIER } from "@common/values";
 
 const REFETCH_INTERVAL = 5_000;
 
@@ -52,12 +53,17 @@ export const useGetEstimateGasInfo = (
         };
       }
 
-      const gasFee = BigNumber(resultGasUsed.gasUsed).multipliedBy(gasPrice).toFixed(0, BigNumber.ROUND_UP);
+      const adjustGasUsedBN = BigNumber(resultGasUsed.gasUsed || DEFAULT_GAS_WANTED).multipliedBy(
+        GAS_WANTED_BUFFER_MULTIPLIER,
+      );
+      const adjustGasUsed = adjustGasUsedBN.toFixed(0, BigNumber.ROUND_DOWN);
+      const adjustGasPriceBN = BigNumber(gasPrice);
+      const gasFee = adjustGasPriceBN.multipliedBy(adjustGasUsed).toFixed(0, BigNumber.ROUND_UP);
 
       return {
         gasFee: Number(gasFee),
-        gasUsed: resultGasUsed.gasUsed,
-        gasWanted: resultGasUsed.gasUsed,
+        gasUsed: Number(adjustGasUsed),
+        gasWanted: Number(adjustGasUsed),
         gasPrice: gasPrice,
         hasError: resultGasUsed.errorMessage !== null,
         simulateErrorMessage: resultGasUsed.errorMessage,
