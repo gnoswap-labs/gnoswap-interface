4e102440c5eba3269aa47529f8ea690317806b14
김진선 <kimjinseon@gimjinseon-ui-MacBookPro.local>
2025-10-23 11:51:52 +0900

    beta stg debug

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 
+const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { i18n } = require("./next-i18next.config");
 
@@ -7,6 +8,9 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   i18n,
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
