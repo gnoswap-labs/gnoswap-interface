89d3a1bee5ac615d4fe9da6b2710d8e48a1a2165
김진선 <kimjinseon@gimjinseon-ui-MacBookPro.local>
2025-10-23 15:53:11 +0900

    next.config.js output add

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -6,6 +6,7 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  output: "standalone",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
