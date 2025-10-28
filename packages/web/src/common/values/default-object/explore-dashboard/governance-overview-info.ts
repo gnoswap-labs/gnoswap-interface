31e4a6165dc6336c04fae87b3c95f66e4cceb303
yjin <124853691+tfrg@users.noreply.github.com>
2025-10-28 10:30:27 +0900

    [GSW-2363] Explore token-amount decimals (#777)
    * feat: [GSW-2363] Explore token-amount decimals
    
    * refactor: [GSW-2363] Prefer `Number.isNaN` over `isNaN`.

---
+++ packages/web/src/common/values/default-object/explore-dashboard/governance-overview-info.ts
@@ -0,0 +1,9 @@
+import { GovernanceOverviewResponse } from "@repositories/dashboard";
+
+export const DEFAULT_GOVERNANCE_OVERVIEW_INFO: GovernanceOverviewResponse = {
+  totalDelegated: 0,
+  holders: 0,
+  passedCount: 0,
+  activeCount: 0,
+  communityPool: 0,
+};
