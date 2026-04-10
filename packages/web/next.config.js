4c7ae9dd7cacb0afa4f3aa1cb675e45f4d70d970
jinoosss <112360739+jinoosss@users.noreply.github.com>
2026-04-10 19:31:04 +0900

    chore: add API rewrites for endpoints

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -6,6 +6,16 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  async rewrites() {
+    const apiUrl = process.env.API_BASE_URL;
+
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${apiUrl}/v1/:path*`,
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
