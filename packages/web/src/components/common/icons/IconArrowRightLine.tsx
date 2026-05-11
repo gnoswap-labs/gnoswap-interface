9d43c7abed71aadbf9da4825f010083311095584
jinoosss <112360739+jinoosss@users.noreply.github.com>
2026-05-11 18:39:20 +0900

    fix: update clip-path to clipPath in IconArrowRightLine component for consistency

--- packages/web/src/components/common/icons/IconArrowRightLine.tsx
+++ packages/web/src/components/common/icons/IconArrowRightLine.tsx
@@ -1,6 +1,6 @@
 const IconArrowRightLine = ({ className }: { className?: string }) => (
   <svg className={className} width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_27862_40876)">
+    <g clipPath="url(#clip0_27862_40876)">
       <path
         d="M10.7814 7.33312L7.20541 3.75712L8.14808 2.81445L13.3334 7.99979L8.14808 13.1851L7.20541 12.2425L10.7814 8.66645H2.66675V7.33312H10.7814Z"
         fill="#596782"
