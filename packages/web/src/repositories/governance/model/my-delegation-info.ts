0c062c5efaee7445242daeb4ef62acba9052fb95
Junghoon Ban <junghoon@onbloc.xyz>
2026-06-09 15:35:13 +0900

    feat(governance): add launchpad fee fields

--- packages/web/src/repositories/governance/model/my-delegation-info.ts
+++ packages/web/src/repositories/governance/model/my-delegation-info.ts
@@ -2,6 +2,8 @@ export interface MyDelegationInfo {
   availableBalance: string;
   claimableRewards: ClaimableRewards[];
   claimableRewardUsd: string;
+  launchpadProtocolFees: ClaimableRewards[];
+  launchpadProtocolFeeUsd: string;
   unDelegatedAmount: string;
   withdrawableAmount: string;
   votingWeight: string;
@@ -16,6 +18,8 @@ export const nullMyDelegationInfo: MyDelegationInfo = {
   availableBalance: "0",
   claimableRewards: [],
   claimableRewardUsd: "0",
+  launchpadProtocolFees: [],
+  launchpadProtocolFeeUsd: "0",
   unDelegatedAmount: "0",
   withdrawableAmount: "0",
   votingWeight: "0",
--- packages/web/src/repositories/governance/response/get-my-delegation-response.ts
+++ packages/web/src/repositories/governance/response/get-my-delegation-response.ts
@@ -4,6 +4,8 @@ export interface GetMyDelegationResponse {
   availableBalance: string;
   claimableRewards: ClaimableRewards[];
   claimableRewardUsd: string;
+  launchpadProtocolFees: ClaimableRewards[];
+  launchpadProtocolFeeUsd: string;
   unDelegatedAmount: string;
   withdrawableAmount: string;
   votingWeight: string;
